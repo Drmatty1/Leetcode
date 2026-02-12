5503ef2c94d76b09f60387c60538e3068be50868
Vishnu Gupta <geekmaths0@gmail.com>
2026-02-12 14:39:02 +0530

    Time: 24 ms (99.63%), Space: 47 MB (62.55%) - LeetHub

--- 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.java
+++ 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.java
@@ -14,16 +14,21 @@ class Solution {
         for( int i=0; i<n; i++ ){
 
             if( ans >= n-i ) break; //early exit
-            
+
             int []map = new int[26];
-            int max = 0 ,min = n ;
+            int max = 0  ;
+            int uniq = 0;
 
             for( int j=i; j<n; j++ ){
+                
                 char c = s.charAt(j);
                 map[c-'a']++;
-                min = findmin(map);
+
+                if( map[c-'a'] == 1 ) uniq++;
                 max = Math.max(max,map[c-'a']);
-                if( min == max ) ans = Math.max(ans,j-i+1);
+
+                if( max*uniq == j-i+1 ) ans = Math.max(ans,j-i+1);
+
             }
         }
         return ans;
