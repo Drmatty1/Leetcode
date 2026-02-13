7546ae3c35deae2b81ee45f9719416971215d2b4
Vishnu Gupta <geekmaths0@gmail.com>
2026-02-13 23:15:36 +0530

    Time: 4 ms (53.3%), Space: 43.1 MB (52.9%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.java
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -1,37 +1,89 @@
 class Solution {
     public String reorganizeString(String s) {
-        int []map = new int[26];
+        int []map = new int[128];
         PriorityQueue<Character> pq = new PriorityQueue<>((c1,c2)->{
-            return Integer.compare(map[c2-'a'],map[c1-'a']);
+            return Integer.compare(map[c2],map[c1]);
         });
+
         // char []arr = s.toCharArray();
-        for( char c : s.toCharArray() ) map[c-'a']++;
-        for( int i=0; i<26; i++  ){
+        for( char c : s.toCharArray() ) map[c]++;
+        for( int i=0; i<128; i++  ){
             if( map[i] != 0 )
-                pq.add((char)(i+'a'));
+                pq.add((char)(i));
         }
 
-        
         StringBuilder ans = new StringBuilder() ;
+        char prev = '$'; 
+        int n = s.length();
+
         while( !pq.isEmpty() ){
 
-            ArrayList<Character> temp = new ArrayList<>();
-            int window = 2;
-            while( window!=0 && !pq.isEmpty() ){
-                char c = pq.poll();
-                window--;
-                map[c-'a']--;
-                ans.append(c);
-                if( map[c-'a'] > 0 ) temp.add(c);
-            }
+            // ArrayList<Character> temp = new ArrayList<>();
+            char c = pq.poll();
+            map[c]--;
+            ans.append(c);
 
-            
-            if( window != 0 && !temp.isEmpty() ) return "";
-            for( char c : temp ) pq.add(c);
+            if( map[prev] > 0 ) pq.add(prev);
 
+            prev = c;
         }
 
-        return new String(ans);
+        return ans.length()==n? (new String(ans)):"" ;
 
     }
 }
+//   without temp array
+/**
+class Solution {
+    public String reorganizeString(String s) {
+        int n = s.length();
+        int[] arr = new int[26];
+
+        // Count frequency of each character
+        for (char c : s.toCharArray()) {
+            arr[c - 'a']++;
+        }
+
+        // Check if any character occurs more than (n+1)/2
+        int maxAllowed = (n + 1) / 2;
+        for (int freq : arr) {
+            if (freq > maxAllowed) return "";
+        }
+
+        // Max-heap based on frequency
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> b.freq - a.freq);
+        for (int i = 0; i < 26; i++) {
+            if (arr[i] > 0) pq.offer(new Pair((char) (i + 'a'), arr[i]));
+        }
+
+        StringBuilder sb = new StringBuilder();
+        Pair prev = new Pair('#', 0);
+
+        while (!pq.isEmpty()) {
+            Pair curr = pq.poll();
+            sb.append(curr.c);
+            curr.freq--;
+
+            // Push previous character back into heap if it still has frequency
+            if (prev.freq > 0) {
+                pq.offer(prev);
+            }
+
+            prev = curr; // update prev outside the if
+        }
+
+        return sb.length() == n ? sb.toString() : "";
+    }
+}
+
+// Pair class to store character and frequency
+class Pair {
+    char c;
+    int freq;
+
+    public Pair(char c, int freq) {
+        this.c = c;
+        this.freq = freq;
+    }
+}
+ */
