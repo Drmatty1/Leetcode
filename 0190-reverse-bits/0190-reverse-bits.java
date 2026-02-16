f1f4a4551d9af64160bcf085ca8d4c20404d8a36
Vishnu Gupta <geekmaths0@gmail.com>
2026-02-16 18:28:08 +0530

    Time: 3 ms (10.46%), Space: 42.8 MB (19.19%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,12 +1,15 @@
 class Solution {
+    Map<Integer,Integer> dp = new HashMap<>();
     public int reverseBits(int n) {
+        if( dp.containsKey(n) ) return dp.get(n);
         StringBuilder ans = new StringBuilder();
         while( ans.length() < 32 ){
             int rem = n%2;
             ans.append((char)(rem+'0'));
             n = n/2;
         }
-        
-        return Integer.parseInt(ans.toString(),2);
+        int rev = Integer.parseInt(ans.toString(),2);
+        dp.put(n, rev);
+        return rev;
     }
 }
