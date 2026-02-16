c0fb194219a91ec71fc2cb442b59ef78424634a0
Vishnu Gupta <geekmaths0@gmail.com>
2026-02-16 18:24:18 +0530

    Time: 2 ms (13.14%), Space: 42.5 MB (52.75%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,12 +1,12 @@
 class Solution {
     public int reverseBits(int n) {
         StringBuilder ans = new StringBuilder();
-        while( n != 0 ){
+        while( ans.length() < 32 ){
             int rem = n%2;
             ans.append((char)(rem+'0'));
             n = n/2;
         }
-        while( ans.length() < 32 ) ans.append('0');
+        
         return Integer.parseInt(ans.toString(),2);
     }
 }
