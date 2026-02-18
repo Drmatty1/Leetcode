17abb9d5c278b7ff8e8510a798c5b47fe9add14d
Vishnu Gupta <geekmaths0@gmail.com>
2026-02-18 22:24:37 +0530

    Time: 9 ms (92.15%), Space: 46.8 MB (94.56%) - LeetHub

---
+++ 2384-largest-palindromic-number/2384-largest-palindromic-number.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String largestPalindromic(String num) {
+        int []no = new int[10];
+        for( char c : num.toCharArray() ) no[c-'0']++;
+
+        StringBuilder ans = new StringBuilder();
+        for( int i = 9; i>=0; i-- ){
+            int count  = no[i]/2;
+            if( i == 0 && ans.length()==0 ) break;
+            while( count-->0 )ans.append( (char)(i+'0') );
+        }
+        int flag = 0;
+        for( int i = 9; i>=0; i-- ){
+            if( no[i] % 2 != 0 ){
+                if( i == 0 && ans.length()==0 ) break;
+                ans.append((char)(i+'0'));
+                System.out.println((char)(i+'0'));
+                flag = 1;
+                break;
+            }
+        }
+        int l = ans.length()-flag;
+        for( int i=l-1; i>=0; i-- ) ans.append(ans.charAt(i));
+        if( ans.length() == 0 ) return "0";
+        return new String(ans);
+    }
+}
