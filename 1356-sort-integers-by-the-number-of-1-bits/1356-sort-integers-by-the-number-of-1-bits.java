08cb30a801e0ee628a50550b7e2fde0e0812558b
Vishnu Gupta <geekmaths0@gmail.com>
2026-02-25 21:27:36 +0530

    Time: 11 ms (35.25%), Space: 46.8 MB (52.92%) - LeetHub

---
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
@@ -0,0 +1,38 @@
+class Solution {
+    void swapArr(int []arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    int partition(int []arr, int s, int e){
+        int i = s-1, j = s, pivot = arr[e];
+        while( j < e){
+            int a = bitCount(arr[j]), b = bitCount(pivot);
+            if( a < b ) swapArr(arr,++i,j);
+            else if( a==b && arr[j]<pivot ) swapArr(arr,++i,j);
+            j++;
+        }
+        swapArr(arr, i+1, e);
+        return i+1;
+    }
+    void QS(int []arr, int s, int e){
+        if(s>e) return ;
+        int pivot = partition(arr,s,e);
+        QS(arr, s, pivot-1);
+        QS(arr, pivot+1, e);
+    }
+    int bitCount(int a){
+        int count = 0;
+        while(a>0){
+            count += (a&1)==1?1:0;
+            a = a>>1;
+        }
+        return count;
+    }
+
+    public int[] sortByBits(int[] arr) {
+        QS(arr,0,arr.length-1);
+        System.out.println(bitCount(116));
+        return arr;
+    }
+}
