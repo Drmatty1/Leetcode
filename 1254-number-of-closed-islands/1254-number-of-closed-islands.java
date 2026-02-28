9c084fc723c4dd6ad7a36641ea74fe358cd93b8f
Vishnu Gupta <geekmaths0@gmail.com>
2026-02-28 21:50:33 +0530

    Time: 2 ms (100%), Space: 46.3 MB (89.8%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.java
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.java
@@ -21,15 +21,38 @@ class Solution {
         }
 
     }
+
+    //dfs without visited array -> Modifying original grid
+    void dfs1(int[][] grid, int i, int j, int []flag){
+        
+        int r = grid.length, c = grid[0].length;
+        if( i>=r || i<0 || j>=c || j<0 )  return ;
+        
+        if( (i == r-1 || i==0 || j == c-1 || j == 0) && grid[i][j]==0 ){
+            flag[0] = 0;
+        }
+        grid[i][j] = 1;
+        
+        for( int[]d : dir ){
+            int a = i+d[0];
+            int b = j+d[1];
+            boolean inGrid = (a>=r || a<0 || b>=c || b<0);
+            if( !inGrid && grid[a][b]==0 ){
+                dfs1(grid,a,b,flag);
+            }
+        }
+
+    }
     public int closedIsland(int[][] grid) {
         int r = grid.length, c = grid[0].length;
-        int[][] visited = new int[r][c];
+        // int[][] visited = new int[r][c];
         int count = 0;
         for( int i=0; i<r; i++ ){
             for(int j=0; j<c; j++){
-                if( visited[i][j] == 0 && grid[i][j]==0 ){
+                if( grid[i][j]==0 ){
                     int[]flag = {1};
-                    dfs(grid,visited,i,j,flag);
+                    // dfs(grid,visited,i,j,flag);
+                    dfs1(grid,i,j,flag);
                     if( flag[0] == 1 ) count++;
                 }
             }
