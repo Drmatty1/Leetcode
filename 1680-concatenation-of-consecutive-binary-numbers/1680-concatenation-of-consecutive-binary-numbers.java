0d01fa8429032c7ecd3831cec8556e90e2d2b72b
Vishnu Gupta <geekmaths0@gmail.com>
2026-02-28 16:18:40 +0530

    Time: 317 ms (23.26%), Space: 42.9 MB (26.74%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.java
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.java
@@ -1,19 +1,36 @@
 class Solution {
     static int[]dp = new int[100001];
+    static int max = -1;
+
+    // int solve(int temp, int n){
+    //     if( temp =  )
+
+    //     if( dp[temp] != 0 ) return dp[temp];
+
+    //     int ans = 0;
+    //     int test = (int)Math.pow(2,17);
+
+    //     while( (test&temp) == 0 ) test = test>>1;
+    //     while( test != 0) {
+    //         ans = ( (ans<<1)+((temp&test)!=0?1:0) )%M ;
+    //         test = test>>1; 
+    //     }
+
+    //     return dp[temp] = ans;
+    // }
+
     public int concatenatedBinary(int n) {
         int M = (int)1e9+7;
-        if( dp[n] != 0 ) return dp[n];
         int ans = 0;
         for( int i=1; i<=n; i++ ){
             int temp = i;
-            int test = (int)Math.pow(2,17);
+            int test = 1<<17;
             while( (test&temp) == 0 ) test = test>>1;
             while( test != 0) {
                 ans = ( (ans<<1)+((temp&test)!=0?1:0) )%M ;
                 test = test>>1; 
             }
         }
-        dp[n] = ans;
         return (ans);
     }
 }
