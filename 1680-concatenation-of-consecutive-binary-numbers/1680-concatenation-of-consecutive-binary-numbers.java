8cf5d145eaa6059abdbba56193626830316dc999
Vishnu Gupta <geekmaths0@gmail.com>
2026-02-28 13:25:19 +0530

    Time: 948 ms (6.98%), Space: 46.2 MB (24.42%) - LeetHub

---
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.java
@@ -0,0 +1,22 @@
+class Solution {
+    StringBuilder getStr(int i){
+        StringBuilder ans = new StringBuilder();
+        while( i !=0 ){
+            ans.append((char)((i&1)+'0'));
+            i = i>>1;
+        }
+        return ans.reverse();
+    }
+    public int concatenatedBinary(int n) {
+        int M = (int)1e9+7;
+        long ans = 0;
+        for( int i=1; i<=n; i++ ){
+            StringBuilder temp = getStr(i);
+            for (int j = 0; j < temp.length(); j++) {
+                char c = temp.charAt(j);
+                ans = ( (ans<<1)+(c-'0') )%M ;
+            }
+        }
+        return (int)(ans%M);
+    }
+}
