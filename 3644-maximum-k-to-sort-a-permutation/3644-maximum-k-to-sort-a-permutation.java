98acf17610e546a6ba009f7c314099b5e9ce3dc2
Vishnu Gupta <geekmaths0@gmail.com>
2026-03-01 13:31:41 +0530

    Time: 2 ms (39.66%), Space: 110.5 MB (87.93%) - LeetHub

--- 3644-maximum-k-to-sort-a-permutation/3644-maximum-k-to-sort-a-permutation.java
+++ 3644-maximum-k-to-sort-a-permutation/3644-maximum-k-to-sort-a-permutation.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int sortPermutation(int[] nums) {
+    int solve1(int[]nums){
         int []arr = nums.clone();
         Arrays.sort(arr);
         int i=0;
@@ -16,5 +16,27 @@ class Solution {
             }
         }
         return ans;
+    }
+    void swap(int[]arr,int i, int j){
+        int t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
+    }
+    int solve2(int []nums){
+        int ans = (1<<29)-1;
+        int i=0, n =  nums.length;
+        while( i<n ){
+            while( nums[i] != nums[nums[i]] ){
+                ans &= nums[i];
+                ans &= nums[nums[i]];
+                swap(nums,nums[i],i);
+            }
+           i++;
+        }
+        return ans;
+    }
+    public int sortPermutation(int[] nums) {
+        int ans = solve2(nums);
+        return ans==((1<<29)-1)?0:ans;
     }
 }
