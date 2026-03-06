ca3e168ae794ec74199a1536577ff5e5b72b0e90
Vishnu Gupta <geekmaths0@gmail.com>
2026-03-07 00:13:29 +0530

    Time: 9 ms (35.59%), Space: 49.5 MB (92.62%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -38,17 +38,28 @@ class Solution {
         return ans;
     }
     List<List<String>> solve2(String[] strs){
+        
         int l = strs.length;
         Map<String,List<Integer>> map = new HashMap<>();
+
         for( int i=0; i<l; i++ ){
-            String temp = sort(strs[i]).toString();
+            
+            // String temp = sort(strs[i]).toString();
+
+            char []arr = strs[i].toCharArray();
+            Arrays.sort(arr);
+            String temp = String.valueOf(arr);
+
             if(!map.containsKey(temp)){
                 map.put(temp, new ArrayList<>());
             }
             map.get(temp).add(i);
         }
+
         List<List<String>> ans= new ArrayList<>();
+
         for( Map.Entry<String,List<Integer>> e : map.entrySet() ){
+
             List<String> temp =  new ArrayList<>();
             for( int I : e.getValue() ){
                 temp.add(strs[I]);
@@ -57,6 +68,7 @@ class Solution {
         }
         return ans;
     }
+
     public List<List<String>> groupAnagrams(String[] strs) {
         return solve2(strs);
     }
