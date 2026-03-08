2bea209fe5553728c1a646c1c1d983a0f117b0ba
Vishnu Gupta <geekmaths0@gmail.com>
2026-03-08 15:57:15 +0530

    Time: 8 ms (96.12%), Space: 47.2 MB (82.28%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -1,7 +1,45 @@
 class Solution {
-    public String minRemoveToMakeValid(String s) {
+    String solve( String s ){
+        StringBuilder ans = new StringBuilder();
+        int open = 0, close=0;
+        for( char c : s.toCharArray() ){
+            if( c == '(' ){
+                open++;
+                ans.append(c);
+            }
+            else if( c == ')'  ){
+                if( open > close ){
+                    close++;
+                    ans.append(c);
+                }
+            }
+            else ans.append(c);
+        }
+        
+        if( close == open ) return ans.toString();
+        
+        open = 0; close = 0;
+        StringBuilder res = new StringBuilder();
+        int l=ans.length();
 
-        // StringBuilder ans = new StringBuilder();
+        for( int i=l-1; i>=0; i-- ){
+            char c = ans.charAt(i);
+            if( c == ')' ){
+                close++;
+                res.append(c);
+            }
+            else if( c == '('  ){
+                if( open < close ){
+                    open++;
+                    res.append(c);
+                }
+            }
+            else res.append(c);
+        }
+
+        return res.reverse().toString();
+    }
+    String solve2( String s ){
         int size = s.length();
         char []ans = new char[size];
         int top=0;
@@ -24,12 +62,10 @@ class Solution {
         if( close == open ) return new String(ans,0,top);
         
         open = 0; close = 0;
-        // StringBuilder res = new StringBuilder();
-        // int l=ans.length();
         char []res = new char[top];
         int i = top-1;
-        int j=i;
-        for( ; j>=0; j-- ){
+
+        for( int j=i; j>=0; j-- ){
             char c = ans[j];
             if( c == ')' ){
                 close++;
@@ -46,5 +82,8 @@ class Solution {
        
         return new String(res,i+1,top-i-1);
 
+    }
+    public String minRemoveToMakeValid(String s) {
+        return solve2(s);
     }
 }
