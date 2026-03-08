03d5ffe4615d3553377ffe5288a7e18faa3928d5
Vishnu Gupta <geekmaths0@gmail.com>
2026-03-08 15:24:12 +0530

    Time: 18 ms (68.03%), Space: 47.2 MB (82.28%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -0,0 +1,42 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        StringBuilder ans = new StringBuilder();
+        int open = 0, close=0;
+        for( char c : s.toCharArray() ){
+            if( c == '(' ){
+                open++;
+                ans.append(c);
+            }
+            else if( c == ')'  ){
+                if( open > close ){
+                    close++;
+                    ans.append(c);
+                }
+            }
+            else ans.append(c);
+        }
+        
+        if( close == open ) return ans.toString();
+        
+        open = 0; close = 0;
+        StringBuilder res = new StringBuilder();
+        int l=ans.length();
+
+        for( int i=l-1; i>=0; i-- ){
+            char c = ans.charAt(i);
+            if( c == ')' ){
+                close++;
+                res.append(c);
+            }
+            else if( c == '('  ){
+                if( open < close ){
+                    open++;
+                    res.append(c);
+                }
+            }
+            else res.append(c);
+        }
+
+        return res.reverse().toString();
+    }
+}
