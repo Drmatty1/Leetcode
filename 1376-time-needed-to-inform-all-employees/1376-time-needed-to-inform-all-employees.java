326a864a32afe66cc6730b7c1797925883458e6e
Vishnu Gupta <geekmaths0@gmail.com>
2026-03-22 14:32:02 +0530

    Time: 83 ms (35.55%), Space: 127.4 MB (57.53%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
@@ -1,17 +1,19 @@
 class Solution {
-    int dfs(List<List<Integer>> adj, int i, boolean []vis, int[]it ){   
-        vis[i] = true;
+    int dfs(List<List<Integer>> adj, int i, int[]it ){   
+        
         int ans= 0;
         for( int n : adj.get(i) ){
-            if( vis[n] == false ){
-                int res = it[i] + dfs(adj,n,vis,it);
-                ans = Math.max(ans, res);
-            }
+        
+            int res = it[i] + dfs(adj,n,it);
+            ans = Math.max(ans, res);
+        
         }
         return ans;
     }
     public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+
         List<List<Integer>> adj = new ArrayList<>();
+
         for( int i=0; i<n; i++ ){
             adj.add(new ArrayList<>());
         }
@@ -20,8 +22,8 @@ class Solution {
             int b = manager[i];
             if( b != -1 ) adj.get(b).add(a);
         }
-        boolean []vis = new boolean[n];
-        int ans = dfs(adj, headID, vis, informTime );
+    
+        int ans = dfs(adj, headID, informTime );
         return  ans;
     }
 }
