70822986f549ddf7b8140e663ff1debecb4f662f
Vishnu Gupta <geekmaths0@gmail.com>
2026-03-24 18:31:44 +0530

    Time: 9 ms (91.18%), Space: 96.8 MB (66.18%) - LeetHub

--- 2906-construct-product-matrix/2906-construct-product-matrix.java
+++ 2906-construct-product-matrix/2906-construct-product-matrix.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int[][] constructProductMatrix(int[][] grid) {
+    int[][] sol(int[][] grid){
         int r = grid.length, c = grid[0].length;
         int M = 12345;
 
@@ -41,5 +41,34 @@ class Solution {
             q = (q*row[i])%M;
         }
         return ans;
+    }
+    int[][] sol2(int[][] grid){
+        int r = grid.length, c = grid[0].length;
+        int M = 12345;
+        int l = r*c;
+        int []p = new int[l];
+        int []s = new int[l];
+
+        p[0] = grid[0][0];
+        for( int i=1; i<l; i++ ){
+            int x = grid[i/c][i%c];
+            p[i] = (int)((1L*p[i-1]*grid[i/c][i%c])%M);   
+        }
+
+        s[l-1] = grid[r-1][c-1];
+        for( int i=l-2; i>=0; i-- ){
+            s[i] = (int)((1L*s[i+1]*grid[i/c][i%c])%M);
+        }
+
+        grid[0][0] = s[1]%M;
+        for( int i=1; i<l-1; i++ ){
+            grid[i/c][i%c] = (int)((1L*p[i-1]*s[i+1])%M);
+        }
+        grid[r-1][c-1] = p[l-2]%M;
+
+        return grid;
+    }
+    public int[][] constructProductMatrix(int[][] grid) {
+        return sol2(grid);
     }
 }
