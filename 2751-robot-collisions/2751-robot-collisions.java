d987d5eb78d4fda27b42e21d8447329753a4fc6e
Vishnu Gupta <geekmaths0@gmail.com>
2026-04-01 23:05:42 +0530

    Time: 70 ms (19.13%), Space: 161.6 MB (5.22%) - LeetHub

---
+++ 2751-robot-collisions/2751-robot-collisions.java
@@ -0,0 +1,51 @@
+class Solution {
+    public List<Integer> survivedRobotsHealths(int[] positions, int[] healths, String directions) {
+        List<Integer> res = new ArrayList<>();
+        int n = positions.length;
+        int [][]ans = new int[n][3];
+        for( int i =0; i<n; i++ ){
+            ans[i][0] = positions[i];
+            ans[i][1] = healths[i];
+            ans[i][2] = directions.charAt(i)=='L'?0:1;
+        }
+        Arrays.sort(ans,(a,b)->Integer.compare(a[0],b[0]));
+        Stack<Integer> st = new Stack<>();
+        for( int i =0; i<n; i++ ){
+            if(ans[i][2] == 0 ){
+                if( !st.isEmpty() ){
+                    int t = st.peek();
+                    if( ans[i][1] == ans[t][1] ){
+                        ans[i][1] = 0;
+                        ans[t][1] = 0;
+                        st.pop();
+                    }
+                    else if( ans[i][1] > ans[t][1] ){
+                        ans[t][1] = 0;
+                        ans[i][1] -= 1;
+                        st.pop();
+                        i--;
+                    }
+                    else if( ans[i][1] < ans[t][1] ){
+                        ans[t][1] -= 1;
+                        ans[i][1] = 0;
+                    }
+                }
+            }
+            else st.push(i);
+        }
+        
+        Map<Integer,Integer> map = new HashMap<>();
+        for( int i=0; i<n; i++ ) map.put(positions[i],i);
+        for( int i=0; i<n; i++ ){
+            int t = map.get(ans[i][0]);
+            positions[t] = ans[i][1];
+        }
+
+        for( int e : positions ){
+            if( e != 0 ){
+                res.add(e);
+            }
+        }
+        return res;
+    }
+}
