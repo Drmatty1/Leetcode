631219e2cc49fc58b75f0d6d6bd76f6c14e6e888
Vishnu Gupta <geekmaths0@gmail.com>
2026-04-07 19:16:48 +0530

    Time: 57 ms (99.3%), Space: 63.4 MB (5.59%) - LeetHub

---
+++ 2069-walking-robot-simulation-ii/2069-walking-robot-simulation-ii.java
@@ -0,0 +1,81 @@
+class Robot {
+
+    int step = 0;
+    int x =0 , y = 0;
+    int width , height ,peri;
+    int d=0;
+    int [][]dir = {{1,0},{0,1},{-1,0},{0,-1}};
+    String []s = { "East", "North", "West", "South" };
+
+    public Robot(int width, int height) {
+        this.width = width;
+        this.height = height;
+        d = 0;
+        peri = width*2 + 2*(height-2);
+    }
+    
+    public void step(int num) {
+        step += num;
+        if( step%peri == 0 ){
+            if(x==0&&y==0) d=3;
+        }
+        step = step % peri;
+        
+    }
+    
+    public int[] getPos() {
+
+        if( step == 0 ) return new int[]{x,y};
+
+        while( step != 0 ){
+
+        
+            int[] vals = {width - x - 1, height - y - 1, x, y};
+            int rem = vals[d];
+
+            int min = Math.min(rem,step);
+            x += min*dir[d][0];
+            y += min*dir[d][1];
+
+            if( step > rem ){
+                step -= rem;
+                d = (d+1)%4;
+            }
+            else step = 0;
+
+        }
+
+        return new int[]{x,y};
+
+    }
+    
+    public String getDir() {
+        if( step == 0 ) return s[d];
+
+        while( step != 0 ){
+
+            int[] vals = {width - x - 1, height - y - 1, x, y};
+            int rem = vals[d];
+
+            int min = Math.min(rem,step);
+            x += min*dir[d][0];
+            y += min*dir[d][1];
+
+            if( step > rem ){
+                step -= rem;
+                d = (d+1)%4;
+            }
+            else step = 0;
+
+        }
+        return s[d];
+    }
+}
+
+/**
+ * Your Robot object will be instantiated and called as such:
+ * Robot obj = new Robot(width, height);
+ * obj.step(num);
+ * int[] param_2 = obj.getPos();
+ * String param_3 = obj.getDir();
+ */
