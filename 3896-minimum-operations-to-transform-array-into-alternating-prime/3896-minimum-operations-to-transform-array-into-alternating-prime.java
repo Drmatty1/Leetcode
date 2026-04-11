94fe9150b0a3642a3b51a479f362bf360977abee
Vishnu Gupta <geekmaths0@gmail.com>
2026-04-11 23:53:33 +0530

    Time: 120 ms (86.2%), Space: 92.6 MB (83.83%) - LeetHub

--- 3896-minimum-operations-to-transform-array-into-alternating-prime/3896-minimum-operations-to-transform-array-into-alternating-prime.java
+++ 3896-minimum-operations-to-transform-array-into-alternating-prime/3896-minimum-operations-to-transform-array-into-alternating-prime.java
@@ -64,7 +64,48 @@ class Solution {
         return ans;
     }
 }
+//   Direct BRUTE FORCE
+/**
+class Solution {
+    public int minOperations(int[] nums) {
+        int n = nums.length;
+        int totalOps =0;
 
+        for(int i=0; i<n;i++){
+            int current = nums[i];
+
+            if(i %2 ==0){
+                while(!isPrime(current)){
+                    current++;
+                    totalOps++;
+                }
+            }else{
+                while(isPrime(current)){
+                    current++;
+                    totalOps++;
+                }
+            }
+        }
+
+
+        return totalOps;
+    }
+
+    private boolean isPrime(int n ){
+        if(n <=1 ) return false;
+        if(n <=3 ) return true;
+        if(n %2 ==0 || n%3 == 0) return false;
+
+        for(int i=5;i*i <=n; i +=6){
+            if(n%i == 0 || n%(i+2) == 0) return false;
+        }
+        return true;
+        }
+    
+
+    
+}
+ */
 
 
 
