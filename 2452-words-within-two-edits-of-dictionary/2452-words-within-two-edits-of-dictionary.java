1840e6dc962822349f35d97af073b47745e303a1
Vishnu Gupta <geekmaths0@gmail.com>
2026-04-22 17:44:04 +0530

    Time: 2 ms (97.79%), Space: 44.3 MB (84.56%) - LeetHub

--- 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.java
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.java
@@ -4,24 +4,25 @@ class Solution {
         if( a.length() != b.length() ) return 3; //>2
         for(int i=0; i<a.length(); i++){
             if(a.charAt(i)!=b.charAt(i)) mm++;
+            if(mm>2)return mm; // pruning
         }
         return mm;
     }
     public List<String> twoEditWords(String[] queries, String[]dictionary ) {
-        Set<String> set = new HashSet<>();
-        for(String s:dictionary) set.add(s);
+        // Set<String> set = new HashSet<>();
+        // for(String s:dictionary) set.add(s);
         List<String> res = new ArrayList<>();
 
         for( String s: queries ){
-            if( set.contains(s) )res.add(s);
-            else {
+            // if( set.contains(s) )res.add(s);
+            // else {
                 for( String t: dictionary ){
                     if(hm(s,t)<=2){
                         res.add(s);
                         break;
                     }
                 }
-            }
+            // }
         }
         return res;
     }
