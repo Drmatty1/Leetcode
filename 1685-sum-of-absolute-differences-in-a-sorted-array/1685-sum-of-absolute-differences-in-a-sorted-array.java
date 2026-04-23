f4a219b5d523ff31660485bec9e4f9123d38d7fe
Vishnu Gupta <geekmaths0@gmail.com>
2026-04-23 18:18:40 +0530

    Time: 4 ms (84.24%), Space: 91.1 MB (34.63%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.java
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.java
@@ -2,13 +2,15 @@ class Solution {
     public int[] getSumAbsoluteDifferences(int[] nums) {
         int n = nums.length;
         // int []p = new int[n];
-        int []s = new int[n];
+        // int []s = new int[n];
 
         // p[0] = nums[0];
-        s[n-1] = nums[n-1];
+        // s[n-1] = nums[n-1];
+        int sum = nums[0];
         for( int i=1; i<n; i++ ){
             // p[i] = p[i-1]+nums[i];
-            s[n-i-1] = s[n-i]+nums[n-i-1];
+            // s[n-i-1] = s[n-i]+nums[n-i-1];
+            sum += nums[i];
         }
 
         int p = 0;
@@ -16,8 +18,8 @@ class Solution {
         for( int i=0; i<n; i++ ){
             int a = i, b = n-i-1;
             int l = i==0?0:p;
+            int r = (i==n-1)?0:(sum-p-nums[i]);
             p += nums[i];
-            int r = (i==n-1)?0:s[i+1];
             nums[i] = a*nums[i]-l + r-b*nums[i];
             
         }
