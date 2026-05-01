727b59efc9b182fb59b0bd4aeae5b1f792ed3971
Vishnu Gupta <geekmaths0@gmail.com>
2026-05-01 18:35:41 +0530

    Time: 22 ms (42.57%), Space: 88.3 MB (73.61%) - LeetHub

--- 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.java
+++ 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.java
@@ -78,7 +78,49 @@ class Solution {
         if(uf.find(0) == uf.find(m*n-1)) return true;
         return false;
     }
+
+    boolean solBFS(int [][]grid){
+
+        int m = grid.length, n = grid[0].length;
+
+        Map<Integer, int[][]> map = new HashMap<>();
+        map.put(1, new int[][]{{0,1},{0,-1}});
+        map.put(2, new int[][]{{1,0},{-1,0}});
+        map.put(3, new int[][]{{0,-1},{1,0}});
+        map.put(4, new int[][]{{0,1},{1,0}});
+        map.put(5, new int[][]{{0,-1},{-1,0}});
+        map.put(6, new int[][]{{0,1},{-1,0}});
+
+        boolean[][] vis = new boolean[m][n];
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{0,0});
+        vis[0][0] = true;
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int i = cur[0], j = cur[1];
+
+            if (i == m-1 && j == n-1) return true;
+
+            for (int[] d : map.get(grid[i][j])) {
+                int ni = i + d[0];
+                int nj = j + d[1];
+
+                if (ni < 0 || nj < 0 || ni >= m || nj >= n || vis[ni][nj]) continue;
+
+                // check if next cell connects back
+                for (int[] back : map.get(grid[ni][nj])) {
+                    if (back[0] == -d[0] && back[1] == -d[1]) {
+                        vis[ni][nj] = true;
+                        q.offer(new int[]{ni,nj});
+                    }
+                }
+            }
+        }
+        return false;
+    }
     public boolean hasValidPath(int[][] grid) {
-        return solDSU(grid);
+        // return solDSU(grid);
+        return solBFS(grid);
     }
 }
