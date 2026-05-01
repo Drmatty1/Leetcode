964a014be71afd66290adba3158d9efab9939d90
Vishnu Gupta <geekmaths0@gmail.com>
2026-05-01 16:05:50 +0530

    Time: 32 ms (5.55%), Space: 142.3 MB (17.05%) - LeetHub

---
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.java
@@ -0,0 +1,48 @@
+class Solution {
+    int [][]dir = {{0,1},{1,0},{0,-1},{-1,0}};
+    int r,c;
+    boolean dfs(char[][]grid, int i, int j, int pi, int pj, char t){
+        
+        grid[i][j] = t ;
+        
+        boolean ans = false;
+
+        char curr = grid[i][j];
+
+        // System.out.println(i+" "+j+" "+t+" "+(int)t);
+
+        for(int []d : dir){
+            int ni = i+d[0];
+            int nj = j+d[1];
+            if(ni>=0 && ni<r && nj>=0 && nj<c ){
+                if(ni == pi && nj == pj) continue;
+               
+                if(grid[ni][nj] == curr) return true;
+                if(grid[ni][nj] == (curr+32)){
+                    ans |= dfs(grid,ni,nj,i,j,t);
+                }
+                
+                    
+            }
+        }
+        // System.out.println(i+" "+j+" "+ans);
+        
+        return ans;
+
+    }
+    public boolean containsCycle(char[][] grid) {
+        r = grid.length;
+        c = grid[0].length;
+        boolean ans= false;
+
+        for(int i=0; i<r; i++){
+            for(int j=0; j<c; j++){
+                char c = grid[i][j];
+                if( c<= 'z' && c>='a' ){
+                    ans |= dfs(grid,i,j,-1,-1,(char)(c-32));
+                }
+            }
+        }
+        return ans;
+    }
+}
