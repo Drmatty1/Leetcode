e360d723c55bc78c36cfb098484263f604d3d2a7
Vishnu Gupta <geekmaths0@gmail.com>
2026-05-27 22:05:41 +0530

    Time: 1 ms (37.7%), Space: 43.5 MB (11.65%) - LeetHub

---
+++ 0068-text-justification/0068-text-justification.java
@@ -0,0 +1,75 @@
+class Solution {
+    String sol(String[] words, int max, int i, int j){
+        
+        int chars = 0;
+        for(int t=i; t<=j; t++){
+            chars += words[t].length();
+        }
+
+        int gaps = j-i;             // ==0 ????
+
+        if( gaps == 0 ){
+            StringBuilder res = new StringBuilder();
+            res.append(words[i]);
+            int spaces = max - chars;
+            while(spaces-->0) res.append(" ");
+            return new String(res);
+        } 
+
+        if( j == words.length-1 ){
+
+            StringBuilder res = new StringBuilder();
+            int rem = max - gaps-chars ;
+
+            for(int t=i; t<j; t++){
+                res.append(words[t]+" ");
+            }
+            res.append(words[j]);
+
+            while(rem -->0){
+                res.append(" ");
+            }
+            return new String(res);
+        }
+
+        int spaces = max - chars;
+        int pergaps = spaces/gaps;
+        int extra = spaces%gaps;
+
+        StringBuilder res = new StringBuilder();
+
+        for(int t=i; t<j; t++){
+            res.append(words[t]);
+            int temp = pergaps + (extra>0?1:0) ;
+            extra -- ;
+            while(temp -->0){
+                res.append(" ");
+            }
+        }
+        res.append(words[j]);
+
+        return new String(res);
+
+    }
+    public List<String> fullJustify(String[] words, int maxWidth) {
+        List<String> ans = new ArrayList<>();
+        
+        int i=0;
+        int n = words.length;
+        
+        while( i<n ){
+            int j=i;
+            int len = words[j].length();
+            while(j+1<n){
+                if(len+1+words[j+1].length() <= maxWidth ){
+                    j++;
+                    len += 1+words[j].length();
+                }
+                else break;
+            }
+            ans.add(sol(words, maxWidth, i, j));
+            i = j+1;
+        }
+        return ans;
+    }
+}
