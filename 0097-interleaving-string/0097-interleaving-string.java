4f5139646ed988c61c01eb0bfd6935d6637f97b7
Vishnu Gupta <geekmaths0@gmail.com>
2026-05-27 23:24:29 +0530

    Time: 1 ms (92.49%), Space: 43.6 MB (18.67%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,4 +1,5 @@
 class Solution {
+
     boolean sol(String s1, String s2, String s3, int i, int j, int k){
 
         int n1 = s1.length(), n2 = s2.length(), n3 = s3.length();
@@ -19,6 +20,7 @@ class Solution {
         }
 
     }
+
 
     boolean sol1(String s1, String s2, String s3, int i, int j, int k, Boolean [][][]dp){
 
@@ -44,13 +46,42 @@ class Solution {
 
     }
 
+
+    boolean sol11(String s1, String s2, String s3, int i, int j, Boolean [][]dp){
+
+        int n1 = s1.length(), n2 = s2.length(), n3 = s3.length();
+
+        if( i == n1 ) 
+            return s2.substring(j).equals(s3.substring(i+j));
+        if( j == n2) 
+            return s1.substring(i).equals(s3.substring(i+j));
+
+        if(dp[i][j] != null) return dp[i][j];
+
+        char c1 = s1.charAt(i), c2 = s2.charAt(j), c3 = s3.charAt(i+j); 
+        if( c1 != c3 ){
+            if(c2 != c3)return dp[i][j] = false;
+            return dp[i][j]= sol11(s1,s2,s3,i,j+1,dp);
+        }
+        else{
+            if(c2 != c3)
+                return dp[i][j]=sol11(s1,s2,s3,i+1,j,dp);;
+            return dp[i][j] = sol11(s1,s2,s3,i,j+1,dp)||sol11(s1,s2,s3,i+1,j,dp);
+        }
+
+    }
+
+
     public boolean isInterleave(String s1, String s2, String s3) {
         int n1 = s1.length(), n2= s2.length(), n3= s3.length();
         if(n1+n2!= n3 )return false;
 
         // return sol(s1,s2,s3,0,0,0);
 
-        Boolean [][][]dp = new Boolean[n1][n2][n3];
-        return sol1(s1,s2,s3,0,0,0,dp);
+        // Boolean [][][]dp = new Boolean[n1][n2][n3];
+        // return sol1(s1,s2,s3,0,0,0,dp);
+
+        Boolean [][]dp = new Boolean[n1][n2];
+        return sol11(s1,s2,s3,0,0,dp);
     }
 }
