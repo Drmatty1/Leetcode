d3db68dc8a5ae6fc4066e902214e0b679ed2d81e
Vishnu Gupta <geekmaths0@gmail.com>
2026-06-13 22:04:20 +0530

    Time: 12 ms (34.56%), Space: 44.1 MB (37.7%) - LeetHub

--- 1114-print-in-order/1114-print-in-order.java
+++ 1114-print-in-order/1114-print-in-order.java
@@ -1,5 +1,8 @@
 class Foo {
-    volatile int turn = 0;
+    // volatile int turn = 0;
+    Semaphore s2 = new Semaphore(0);
+    Semaphore s3 = new Semaphore(0);
+
     public Foo() {
         
     }
@@ -7,23 +10,29 @@ class Foo {
     public void first(Runnable printFirst) throws InterruptedException {
         
         // printFirst.run() outputs "first". Do not change or remove this line.
-        turn = 0;
+        
         printFirst.run();
-        turn = 1;
+        // turn = 1;
+        s2.release();
     }
 
     public void second(Runnable printSecond) throws InterruptedException {
         
         // printSecond.run() outputs "second". Do not change or remove this line.
-        while(turn != 1);
+
+        // while(turn != 1);
+        s2.acquire();
         printSecond.run();
-        turn = 2;
+        // turn = 2;
+        s3.release();
     }
 
     public void third(Runnable printThird) throws InterruptedException {
         
         // printThird.run() outputs "third". Do not change or remove this line.
-        while(turn != 2);
+
+        // while(turn != 2);
+        s3.acquire();
         printThird.run();
 
     }
