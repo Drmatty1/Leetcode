0ea47a418e4155a12573a11f246bfd7b452cbc5d
Vishnu Gupta <geekmaths0@gmail.com>
2026-06-14 14:35:50 +0530

    Time: 18 ms (94.15%), Space: 138.5 MB (11.11%) - LeetHub

---
+++ 2197-replace-non-coprime-numbers-in-array/2197-replace-non-coprime-numbers-in-array.java
@@ -0,0 +1,42 @@
+class Solution {
+    int hcf(int a, int b){
+        if(b==0)return a;
+        return hcf(b,a%b);
+    }
+    long lcm(int a, int b){
+        return 1L*a*b/hcf(a,b);
+    }
+    boolean ncprm(int a, int b){
+        return hcf(a,b)>1;
+    }
+
+    List<Integer> sol(int[] nums) {
+        Stack<Integer> st = new Stack<>();
+        for(int e: nums){
+            int fusion  = e;
+            while( !st.isEmpty() && ncprm(fusion,st.peek()) ){
+                fusion = (int)lcm(fusion,st.pop());
+            }
+            st.add(fusion);
+        }
+        return new ArrayList<>(st);
+    }
+
+    public List<Integer> replaceNonCoprimes(int[] nums) {
+        
+        int []st = new int[nums.length+1];
+        int top = -1;
+
+        for(int e: nums){
+            int fusion  = e;
+            while( (top!=-1) && ncprm(fusion,st[top]) ){
+                fusion = (int)lcm(fusion,st[top--]);
+            }
+            st[++top] = fusion ;
+        }
+        List<Integer> res = new ArrayList<>();
+        for(int k=0; k<=top; k++)res.add(st[k]);
+
+        return res;
+    }
+}
