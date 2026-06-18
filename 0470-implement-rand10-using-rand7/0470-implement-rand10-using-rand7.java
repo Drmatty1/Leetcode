21c39e11e5f9da53562656dabb81e29ce2798537
Vishnu Gupta <geekmaths0@gmail.com>
2026-06-18 23:34:23 +0530

    Time: 7 ms (12.53%), Space: 51.8 MB (25.3%) - LeetHub

--- 0470-implement-rand10-using-rand7/0470-implement-rand10-using-rand7.java
+++ 0470-implement-rand10-using-rand7/0470-implement-rand10-using-rand7.java
@@ -5,9 +5,22 @@
  */
 class Solution extends SolBase {
     public int rand10() {
-        while(true){
-            int a = (rand7()-1)*7 + rand7() ;
-            if( a <= 40 ) return (a%10)+1;
+        // while(true){
+        //     int a = (rand7()-1)*7 + rand7() ;
+        //     if( a <= 40 ) return (a%10)+1;
+        // }
+        int a = rand7();
+        while(a == 7 ) a = rand7();
+        if( a <= 3 ){
+            int b = rand7();
+            while( b>5 ) b = rand7();
+            return b;
         }
+        else{
+            int b = rand7();
+            while( b>5 ) b = rand7();
+            return b+5;
+        }
+
     }
 }
