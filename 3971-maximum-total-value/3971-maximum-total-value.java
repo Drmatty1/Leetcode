3d91d114eb4e444d3ba6eb8ca04cce9d399f11da
Vishnu Gupta <geekmaths0@gmail.com>
2026-06-21 09:54:25 +0530

    Time: 140 ms (5.76%), Space: 106.7 MB (19.78%) - LeetHub

---
+++ 3971-maximum-total-value/3971-maximum-total-value.java
@@ -0,0 +1,44 @@
+class Solution {
+    long[] count(int []v, int[]dec , long m){
+        int s =  v.length;
+        long sum = 0, ct = 0;
+        for(int i=0; i<s; i++){
+            long a = v[i];
+            long d = -dec[i];
+            long n = (m-a)/d+1;
+            if( m > a ) n = 0;
+            if( m == a ) n = 1;
+            sum += n * (2 * a + (n - 1) * d) / 2;
+            ct += n;
+        }
+        return new long[]{ct, sum};
+    }
+    public int maxTotalValue(int[] value, int[] decay, int m) {
+        int mod = 1000000007;
+        int n =  value.length;
+        long l = 1, u = (long)(1e9);
+        long []ps = {0,0};
+        long psMid= -1;
+        
+        while( l <= u){
+            long mid = (u-l)/2+l;
+            long []res = count(value,decay,mid);
+            
+            if(res[0] >= m ){
+                ps = res;
+                psMid = mid;
+                l = mid+1;
+            }
+            else {
+                u = mid-1;
+            }
+        }
+
+        if(psMid == -1){
+            // System.out.println("hi");
+            return (int)((count(value,decay,0)[1])%mod);
+        }
+        long ans = (ps[1] - psMid*(ps[0]-m))%mod;
+        return (int)ans;
+    }
+}
