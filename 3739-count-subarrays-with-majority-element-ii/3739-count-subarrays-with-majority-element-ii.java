7115bdaa3fb0ef4c07142e13bbaec0141fceb00f
Vishnu Gupta <geekmaths0@gmail.com>
2026-06-25 18:02:11 +0530

    Time: 5 ms (79.37%), Space: 89.5 MB (65.08%) - LeetHub

--- 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.java
+++ 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.java
@@ -1,5 +1,6 @@
 class Solution {
-    public long countMajoritySubarrays(int[] nums, int target) {
+
+    long sol1(int[] nums, int target) {
         int n = nums.length;
         int []pre = new int[n+1];
 
@@ -31,6 +32,52 @@ class Solution {
         }
 
         return ans;
+
+    }
+    long sol11(int[] nums, int target) {
+        int n = nums.length;
+        int []pre = new int[n+1];
+
+        for(int i=0; i<n; i++){
+            pre[i+1] = pre[i] + (target==nums[i]?1:(-1));
+        }
+
+        // for(int e: pre) System.out.print(e+" ");
+
+        long ans = 0 ;
+        long smallerCount = 0;
+        // Map <Integer,Integer> map = new HashMap<>();
+        int []map = new int[2*n+1];
+        // map.put(0,1);
+        map[0+n] = 1;
+
+        // 0  -1   0   1   0     Pre array
+        // 0   1   2   3   4     index 
+        for(int i=1; i<=n; i++){
+
+            if( pre[i] > pre[i-1] ){
+                // smallerCount += map.getOrDefault(pre[i-1],0);
+                smallerCount += map[n + pre[i-1]];
+            }
+            else{
+                // smallerCount -= map.getOrDefault(pre[i],0);
+                smallerCount -= map[n + pre[i]];
+            }
+
+            ans += smallerCount;
+            
+            // map.put( pre[i], map.getOrDefault(pre[i],0)+1 );
+            map[n + pre[i]] ++;
+
+        }
+
+        return ans;
+
+    }
+
+    public long countMajoritySubarrays(int[] nums, int target) {
+
+        return sol11(nums, target);
 
     }
 }
