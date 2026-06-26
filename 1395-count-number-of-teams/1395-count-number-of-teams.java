b62b4bfdbb597c3300d01f8ec08ba86d1b4fc617
Vishnu Gupta <geekmaths0@gmail.com>
2026-06-26 17:58:38 +0530

    Time: 7 ms (97.81%), Space: 47.3 MB (7.3%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.java
+++ 1395-count-number-of-teams/1395-count-number-of-teams.java
@@ -1,5 +1,6 @@
 class Solution {
-    int sol(int []arr, int prev, int idx, int k, int [][][]dp, boolean [][][]vis){
+
+    int incSubSeq(int []arr, int prev, int idx, int k, int [][][]dp, boolean [][][]vis){
         if( k == -1 )return 1;
         if( idx == arr.length ) return 0;
 
@@ -9,16 +10,16 @@ class Solution {
         //take
         int a = 0;
         if( prev == -1 || arr[idx] > arr[prev] ){
-            a = sol(arr, idx, idx+1, k-1, dp, vis);
+            a = incSubSeq(arr, idx, idx+1, k-1, dp, vis);
         }
 
         //skip
-        a += sol(arr, prev, idx+1, k, dp, vis);
+        a += incSubSeq(arr, prev, idx+1, k, dp, vis);
 
         vis[prev+1][idx][k] = true;
         return  dp[prev+1][idx][k] = a;
     }
-    int sol1(int []arr, int prev, int idx, int k, int[][][]dp, boolean [][][]vis){
+    int decSubSeq(int []arr, int prev, int idx, int k, int[][][]dp, boolean [][][]vis){
         if( k == -1 ) return 1;
         if( idx == arr.length ) return 0;
 
@@ -28,28 +29,89 @@ class Solution {
         //take
         int a = 0;
         if(  prev == -1 || arr[idx] < arr[prev]  ){
-            a = sol1(arr, idx, idx+1, k-1, dp, vis);
+            a = decSubSeq(arr, idx, idx+1, k-1, dp, vis);
         }
 
         //skip
-        a += sol1(arr, prev, idx+1, k, dp, vis);
+        a += decSubSeq(arr, prev, idx+1, k, dp, vis);
 
         vis[prev+1][idx][k] = true;
         return  dp[prev+1][idx][k] = a;
     }
-
-    public int numTeams(int[] rating) {
+    int sol(int[] rating) {
         
         int n = rating.length;
 
         int [][][]dp = new int[n+1][n][3];
         boolean [][][]vis = new boolean[n+1][n][3];
-        int a = sol(rating,-1,0,2,dp,vis);
+        int a = incSubSeq(rating,-1,0,2,dp,vis);
         
         // dp = new int[n+1][n][4];
         vis = new boolean[n+1][n][3];
-        int b = sol1(rating,-1,0,2,dp,vis);
+        int b = decSubSeq(rating,-1,0,2,dp,vis);
 
         return a+b;
+    } 
+
+    class Fenwick{
+        int []fenwick;
+        int n ;
+        Fenwick(int s){
+            n = s+1;
+            fenwick = new int[n];
+        }
+
+        void update(int idx, int val){
+            idx = idx+1;
+            while(idx < n){
+                fenwick[idx] += val;
+                idx += idx & (-idx);
+            }
+        }
+        int find( int r ){
+            r = r+1;
+            int sum = 0;
+            while( r > 0 ){
+                sum += fenwick[r];
+                r -= r & (-r);
+            }
+            return sum;
+        }
+    }
+
+    public int numTeams(int[] rating) {
+        
+        int n = rating.length;
+        
+        int len = 100001;
+
+        Fenwick f1 = new Fenwick(len);
+        Fenwick f2 = new Fenwick(len);
+
+        for(int i=0; i<n; i++){
+            int idx = rating[i];
+            f2.update(idx, 1);
+        }
+
+        int ans = 0;
+
+        for(int i=0; i<n; i++){
+            int idx = rating[i];
+            f2.update(idx, -1);
+
+            int leftSmall = f1.find(idx-1);
+            int leftLarge = i - f1.find(idx);
+
+            int rightsmall = f2.find(idx-1);
+            int rightlarge = (n - 1 - i) - f2.find(idx);
+
+            ans += leftSmall*rightlarge + leftLarge*rightsmall;
+
+            f1.update(idx,1);
+
+        }
+
+        return ans;
+
     } 
 }
