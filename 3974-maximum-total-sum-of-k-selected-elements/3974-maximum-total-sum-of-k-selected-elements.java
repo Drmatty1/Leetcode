5ed0f59db3b90389212f91b874d9cc6eeddbcf18
Vishnu Gupta <geekmaths0@gmail.com>
2026-06-28 16:36:48 +0530

    Time: 50 ms (33.25%), Space: 149 MB (48.83%) - LeetHub

--- 3974-maximum-total-sum-of-k-selected-elements/3974-maximum-total-sum-of-k-selected-elements.java
+++ 3974-maximum-total-sum-of-k-selected-elements/3974-maximum-total-sum-of-k-selected-elements.java
@@ -1,9 +1,11 @@
 class Solution {
     public long maxSum(int[] nums, int k, int mul) {
         Arrays.sort(nums);
+
         long sum = 0;
         int n = nums.length;
         int j = n-1;
+
         while( k > 0 && mul > 0 ){
             sum += 1L*mul*nums[j];
             j--;
@@ -18,14 +20,6 @@ class Solution {
             k--;
         }
        
-        int i = 0;
-        while( k > 0 && i <= j ){
-            sum += 1L*mul*nums[i];
-            i++;
-            mul--;
-            k--;
-        }
-
         return sum;
     }
 }
