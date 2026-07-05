3f285000688ecea7cc6ecb9fa9e429f22780f834
Vishnu Gupta <geekmaths0@gmail.com>
2026-07-05 17:48:21 +0530

    Time: 31 ms (63.55%), Space: 49.9 MB (5.88%) - LeetHub

---
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.java
@@ -0,0 +1,29 @@
+class Solution {
+    int[] eqn(int []p1, int []p2){
+        int a  = p1[0], b = p1[1], c = p2[0], d = p2[1];
+        return new int[]{c-a,b-d,(a-c)*b + (d-b)*a};
+    }
+    public int maxPoints(int[][] points) {
+        int ans = 1, n = points.length;
+        Set<String> vis = new HashSet<>();
+
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+
+                int []eqn = eqn(points[i],points[j]);
+                String key = eqn[0]+"-"+eqn[1]+"-"+eqn[2];
+                if( vis.contains(key) ) continue;
+
+                int res = 0;
+                for(int k = j+1; k<n; k++){
+                    int x = points[k][0], y = points[k][1];
+                    int t = y*eqn[0]+x*eqn[1]+eqn[2];
+                    if( t == 0 ) res ++;
+                }
+                vis.add(key);
+                ans = Math.max(ans,res+2);
+            }
+        }
+        return ans;
+    }
+}
