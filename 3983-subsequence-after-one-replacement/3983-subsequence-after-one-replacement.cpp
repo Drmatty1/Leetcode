0e1d60a6ec4739dfaf4b4ee00ab61a68b3f77223
Vishnu Gupta <geekmaths0@gmail.com>
2026-07-05 11:36:09 +0530

    Time: 687 ms (5.04%), Space: 196.6 MB (5.56%) - LeetHub

--- 3983-subsequence-after-one-replacement/3983-subsequence-after-one-replacement.cpp
+++ 3983-subsequence-after-one-replacement/3983-subsequence-after-one-replacement.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int n,m;
-    unordered_map<int,unordered_map<int,unordered_map<int,int>>> dp;
+    unordered_map<string,int> dp;
     bool f(int i,int j,int c,string& s, string& t){
         if(i==n) return true;
         if(j==m) return false;
         
         bool ans=false;
-        if(dp.find(i)!=dp.end() && dp[i].find(j)!=dp[i].end() && 
-         dp[i][j].find(c)!=dp[i][j].end() ) return dp[i][j][c];
+        string key = to_string(i) + "-" + to_string(j) + "-" + to_string(c);
+
+        if(dp.find(key)!=dp.end() ) return dp[key];
 
         if(m-j<n-i) return false;
         if(s[i]==t[j]){
@@ -20,15 +21,14 @@ public:
         else{
                 ans=ans|f(i,j+1,c,s,t);
             }
-        return dp[i][j][c]=ans;
+        return dp[key]=ans;
     }
     
     bool canMakeSubsequence(string s, string t) {
-        unordered_map<char,int> mpp;
+
         n=s.size();
         m=t.size();
-        
-        
+    
         bool b=f(0,0,1,s,t);
     
         return b;
