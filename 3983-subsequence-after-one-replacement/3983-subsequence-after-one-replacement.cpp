e7c7ce74755a4acaea22414b1b3038758094c31d
Vishnu Gupta <geekmaths0@gmail.com>
2026-07-05 11:31:21 +0530

    Time: 750 ms (5.04%), Space: 414.2 MB (5.56%) - LeetHub

--- 3983-subsequence-after-one-replacement/3983-subsequence-after-one-replacement.cpp
+++ 3983-subsequence-after-one-replacement/3983-subsequence-after-one-replacement.cpp
@@ -1,13 +1,36 @@
 class Solution {
 public:
-    bool canMakeSubsequence(string s, string t) { 
-        int i=0,j=0, m=s.size();
-        for(auto c:t){
-            if(s[i]==c) i++;
-            i=max(i,j+1);
-            if(s[j]==c)  j++;
-            if(i==m || j==m ) return true;
-        }
-        return false;
+    int n,m;
+    unordered_map<int,unordered_map<int,unordered_map<int,int>>> dp;
+    bool f(int i,int j,int c,string& s, string& t){
+        if(i==n) return true;
+        if(j==m) return false;
+        
+        bool ans=false;
+        if(dp.find(i)!=dp.end() && dp[i].find(j)!=dp[i].end() && 
+         dp[i][j].find(c)!=dp[i][j].end() ) return dp[i][j][c];
+
+        if(m-j<n-i) return false;
+        if(s[i]==t[j]){
+                ans=ans|f(i+1,j+1,c,s,t);
+            }
+        else if(c==1){
+                ans=ans|f(i+1,j+1,0,s,t)|f(i,j+1,c,s,t);
+            }
+        else{
+                ans=ans|f(i,j+1,c,s,t);
+            }
+        return dp[i][j][c]=ans;
+    }
+    
+    bool canMakeSubsequence(string s, string t) {
+        unordered_map<char,int> mpp;
+        n=s.size();
+        m=t.size();
+        
+        
+        bool b=f(0,0,1,s,t);
+    
+        return b;
     }
 };
