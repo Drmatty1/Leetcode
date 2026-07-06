074d3b0facc620d4552e7f6b1b539ac001764e5b
Vishnu Gupta <geekmaths0@gmail.com>
2026-07-06 13:19:51 +0530

    Time: 6 ms (93.21%), Space: 46.4 MB (62.65%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.java
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.java
@@ -3,7 +3,7 @@ class Solution {
         if(b[0] >= a[0] && b[1] <= a[1] )return true;
         return false;
     }
-    public int removeCoveredIntervals(int[][] intervals) {
+    int sol1(int[][] intervals) {
         Arrays.sort(intervals, (a,b) -> {
             if(a[0] == b[0]) return b[1]-a[1];
             return a[0]-b[0];
@@ -19,5 +19,23 @@ class Solution {
             }
         }
         return st.size();
+    }
+
+    public int removeCoveredIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a,b) -> {
+            if(a[0] == b[0]) return b[1]-a[1];
+            return a[0]-b[0];
+        });
+
+        int []last = {-1,-1};
+
+        int count = 0;
+        for(int []i: intervals){
+            if( ! contains(last, i)){
+                count++;
+                last = i;
+            }
+        }
+        return count;
     }
 }
