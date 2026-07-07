2b3dab28cc30aa432987988c5ffcfd4407e0a89f
Vishnu Gupta <geekmaths0@gmail.com>
2026-07-07 12:35:48 +0530

    Time: 17 ms (86.44%), Space: 130.5 MB (30.51%) - LeetHub

--- 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.java
+++ 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.java
@@ -35,8 +35,7 @@ class Solution {
         return merge(curr, next, nextCnt );
 
     }
-
-    public int[] sumAndMultiply(String s, int[][] queries) {
+    int[] sparseTableSoln(String s, int[][] queries) {
         int n = s.length();
         int []arr = new int[n];
         
@@ -85,6 +84,65 @@ class Solution {
         }
 
         return ans;
+    }
+
+
+    // m-2 best simple
+    public int[] sumAndMultiply(String s, int[][] queries) {
+        int m = s.length();
+        long mod = 1_000_000_007L;
+
+        // Precompute powers of 10 modulo 10^9 + 7
+        long[] pow10 = new long[m + 1];
+        pow10[0] = 1;
+        for (int i = 1; i <= m; i++) {
+            pow10[i] = (pow10[i - 1] * 10) % mod;
+        }
+
+        // Prefix arrays (1-indexed to easily handle L-1 == -1)
+        long[] prefVal = new long[m + 1];
+        int[] prefCount = new int[m + 1];
+        long[] prefSum = new long[m + 1];
+
+        for (int i = 0; i < m; i++) {
+            int digit = s.charAt(i) - '0';
+            
+            // If it's a non-zero digit, shift the previous value and add it
+            if (digit > 0) {
+                prefVal[i + 1] = (prefVal[i] * 10 + digit) % mod;
+                prefCount[i + 1] = prefCount[i] + 1;
+            } else {
+                prefVal[i + 1] = prefVal[i];
+                prefCount[i + 1] = prefCount[i];
+            }
+            
+            // Standard prefix sum includes all digits (0s don't affect sum)
+            prefSum[i + 1] = prefSum[i] + digit;
+        }
+
+        int q = queries.length;
+        int[] answer = new int[q];
+
+        for (int i = 0; i < q; i++) {
+            int l = queries[i][0];
+            int r = queries[i][1];
+
+
+            int nonZeroDigits = prefCount[r+1] - prefCount[l];
+            int R = r+1, L = l;
+
+            // 2. Extract the concatenated value x using the rolling hash formula
+            // We add 'mod' before taking the remainder to prevent negative results in Java
+            long x = (prefVal[R] - (prefVal[L] * pow10[nonZeroDigits]) % mod + mod) % mod;
+
+            // 3. Get the sum of digits in the range
+            long sum = prefSum[R] - prefSum[L];
+
+            // 4. Compute the final answer for the query
+            answer[i] = (int) ((x * sum) % mod);
+        }
+
+        return answer;
     }
 }
 
