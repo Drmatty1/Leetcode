77a3c73dfb9dee3ccea063dc1bd6ed3ffb337295
Vishnu Gupta <geekmaths0@gmail.com>
2026-07-09 11:06:11 +0530

    Time: 1872 ms (5.94%), Space: 146.1 MB (98.02%) - LeetHub

---
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.java
@@ -0,0 +1,51 @@
+class Solution {
+    boolean check(int u, int v, int[] maxJump){
+        if( u > v ) return check(v, u, maxJump);
+        
+        if( maxJump[u] > v ) return true;
+        if(maxJump[u] == u+1 ) return false;
+        return check(maxJump[u]-1, v, maxJump);
+
+    }
+    public boolean[] pathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries) {
+
+        int []maxJump = new int[n];
+       
+        int j=0;
+        for(int i=0; i<n; i++){
+            while( j < n && nums[j]-nums[i] <= maxDiff ) j++;
+            maxJump[i] = j;
+        }
+
+        for(int e: maxJump) System.out.print(e+" ");
+
+        int m = queries.length;
+        boolean []ans = new boolean[m];
+        for(int k=0; k<m; k++){
+            ans[k] = check(queries[k][0],queries[k][1],maxJump);
+        }
+
+        return ans;
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
