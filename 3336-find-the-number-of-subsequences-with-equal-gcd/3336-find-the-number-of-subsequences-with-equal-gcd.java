c0593344a7d266e3f010deb290e28cf005e02fd7
Vishnu Gupta <geekmaths0@gmail.com>
2026-07-14 12:31:05 +0530

    Time: 173 ms (69.44%), Space: 49.1 MB (58.33%) - LeetHub

--- 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.java
+++ 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.java
@@ -56,7 +56,6 @@ class Solution {
             dp[n][j][j] = 1;
         }
         
-
         for(int i=n-1; i>=0; i-- ){
             
             for(int g1 = maxGcd-1; g1>=0; g1--){
@@ -84,6 +83,57 @@ class Solution {
         return dp[0][0][0];
     }
 
+    int sol3(int[]nums){
+
+        //max Gcd
+        int maxGcd = 1;
+        for(int e:nums) maxGcd = Math.max(maxGcd,e);
+        maxGcd = maxGcd+1;
+
+        // gcd lookUp
+        int[][]gcd_dp = new int[maxGcd][maxGcd];
+        for(int i=0; i<maxGcd; i++ ) 
+            for(int j=0;j<maxGcd;j++)
+                gcd_dp[i][j]=gcd(i,j);
+        
+
+        int n = nums.length;
+        int [][]next = new int[maxGcd][maxGcd];
+       
+        for(int j=1; j<maxGcd; j++){
+            next[j][j] = 1;
+        }
+        
+        for(int i=n-1; i>=0; i-- ){
+            int [][]curr = new int[maxGcd][maxGcd];
+
+            for(int g1 = maxGcd-1; g1>=0; g1--){
+                int ng1 = gcd_dp[g1][nums[i]];
+
+                for(int g2 = maxGcd-1; g2>=0; g2--){
+                    int ng2 = gcd_dp[g2][nums[i]];
+
+                    int ans = next[g1][g2];
+
+                    //add elem i, in seq1
+                    ans = (ans+next[ng1][g2])%mod;
+
+                    // add to seq2
+                    ans = (ans+next[g1][ng2])%mod;
+
+                    curr[g1][g2] = ans;
+
+                }
+
+            }
+
+            next = curr;
+
+        }
+
+        return next[0][0];
+    }
+
     public int subsequencePairCount(int[] nums) {
         int n = nums.length;
 
@@ -91,6 +141,7 @@ class Solution {
         // return sol(0,0,0,nums);
         // return sol1(0,0,0,nums);
         
-        return sol2(nums);
+        // return sol2(nums);
+        return sol3(nums);
     }
 }
