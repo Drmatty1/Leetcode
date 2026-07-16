1362078047d5c338a9e7ed43bc4911ec8b802a3c
Vishnu Gupta <geekmaths0@gmail.com>
2026-07-16 16:47:44 +0530

    Time: 127 ms (42.86%), Space: 139.4 MB (28.57%) - LeetHub

--- 3469-find-minimum-cost-to-remove-array-elements/3469-find-minimum-cost-to-remove-array-elements.java
+++ 3469-find-minimum-cost-to-remove-array-elements/3469-find-minimum-cost-to-remove-array-elements.java
@@ -1,11 +1,12 @@
 class Solution {
     Map<Long, Integer> dp = new HashMap<>();
+    Integer[][]dp1 ;
     int sol(int i, int carry, int[] nums){
 
         if(i>=nums.length) return carry;
         if( i == nums.length-1) return Math.max(nums[i],carry);
 
-        long key = (((long) i) << 32) | (carry & 0xffffffffL);
+        long key = (((long) i) << 32) | (carry);
         if(dp.containsKey(key)) return dp.get(key);
 
         //c1
@@ -21,11 +22,39 @@ class Solution {
         int ans = Math.min(a,Math.min(b,c));
         dp.put(key, ans);
         return ans;
+    }
+
+    int sol1(int i, int j, int[] nums){
+
+        if(i>=nums.length) return nums[j];
+        if( i == nums.length-1) return Math.max(nums[i],nums[j]);
+
+        // long key = (((long) i) << 32) | (j);
+        if(dp1[i][j] != null ) return dp1[i][j];
+
+        //c1
+        int a = Math.max(nums[j],nums[i]) + sol1(i+2,i+1,nums);
+
+        //c2
+        int b = Math.max(nums[j],nums[i+1]) + sol1(i+2,i,nums);
+
+        //c3
+        int c = Math.max(nums[i], nums[i+1]) + sol1(i+2,j,nums);
+
+
+        int ans = Math.min(a,Math.min(b,c));
+        
+        return dp1[i][j] = ans;
 
     }
 
+
     public int minCost(int[] nums) {
-        dp = new HashMap<>();
-        return sol(1,nums[0],nums);
+        // dp = new HashMap<>();
+        // return sol(1,nums[0],nums);
+
+        int n = nums.length;
+        dp1 = new Integer[n+2][n];
+        return sol1(1,0,nums);
     }
 }
