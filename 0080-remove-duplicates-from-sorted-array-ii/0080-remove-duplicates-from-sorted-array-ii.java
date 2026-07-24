dedac626daa5e53989386ab0e1fc504c19e87128
Vishnu Gupta <geekmaths0@gmail.com>
2026-07-24 19:36:15 +0530

    Time: 1 ms (18.18%), Space: 48.7 MB (69.91%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,5 +1,47 @@
 class Solution {
-    public int removeDuplicates(int[] nums) {
+    int sol1(int[] nums) {
+        int n = nums.length;
+        if (n <= 2) return n;
+
+        int i = 0; // Points to the start of the current unique number group
+        int max = 100000;
+        int temp = max;
+        int count = 0;
+        int currentCount = 1; // Tracks occurrences of the number at nums[i]
+        
+        // PASS 1: Mark duplicates exceeding 2 occurrences with a placeholder
+        // We use a clean linear scan to maintain O(n) time complexity
+        for (int j = 1; j < n; j++) {
+            if (nums[j] == nums[i]) {
+                currentCount++;
+                if (currentCount > 2) {
+                    count++;
+                    nums[j] = temp++; // Mark as an unwanted duplicate
+                }
+            } else {
+                // Found a new distinct number! 
+                // This is where your commented logic 'i = j' properly belongs.
+                i = j; 
+                currentCount = 1; // Reset the frequency tracker for the new number
+            }
+        }
+
+        // PASS 2: Shift valid elements forward (Your exact original cleanup loop)
+        i = 0;
+        int j = 0;
+        while (j < n) {
+            if (nums[j] < max) {
+                nums[i] = nums[j];
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+
+        return n - count;
+    }
+    int sol2(int[] nums) {
         int i=0, j = 2, n = nums.length;
         int max = 100000;
         int temp = max;
@@ -7,12 +49,6 @@ class Solution {
 
         if(n <= 2 )return n;
 
-        // if( nums[2] != nums[0] ){
-        //     if(nums[1] != nums[0]){
-        //         j = 1;
-        //     }
-        //     else j=2;
-        // }
 
         while(j<n){
             if(nums[j] == nums[i]) {
@@ -42,6 +78,24 @@ class Solution {
         }
 
         return n-count;
+
+    }
+    int solOP(int[] nums) {
+        if (nums.length <= 2) return nums.length;
+
+        int k = 2;
+
+        for (int i = 2; i < nums.length; i++) {
+            if (nums[i] != nums[k - 2]) {
+                nums[k++] = nums[i];
+            }
+        }
+
+        return k;
+    }
+    public int removeDuplicates(int[] nums) {
+        
+        return sol2(nums);
 
     }
 }
