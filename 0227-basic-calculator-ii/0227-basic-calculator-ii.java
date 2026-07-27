92413c082fccf0a22f65448f0b9d571ab2dd5f2e
Vishnu Gupta <geekmaths0@gmail.com>
2026-07-27 23:12:07 +0530

    Time: 19 ms (74.34%), Space: 47.8 MB (81.63%) - LeetHub

---
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int calculate(String s) {
+        s = s.replace(" ", "");
+        Stack<Integer> st = new Stack<>();
+        char sign = '+';
+        int num = 0;
+        int n = s.length();
+
+        for(int i=0; i<=n; i++){
+            char c = (i<n)?s.charAt(i):'Q';
+
+            if( i < n && c<='9' && c >= '0' ){
+                num = num*10 + (c-'0');
+            }
+            else{
+                switch (sign){
+                    case '+' : st.push(num); break;
+                    case '-' : st.push(-num); break;
+                    case '*' : st.push(st.pop()*num); break;
+                    case '/' : st.push(st.pop()/num); break;
+                }
+                
+                sign = c;
+                num = 0;
+            }
+        }
+
+        int sum = 0;
+        while (!st.isEmpty()) sum += st.pop();
+        return sum;
+    }
+}
