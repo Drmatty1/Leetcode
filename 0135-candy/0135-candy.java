548e6f733d51147af86f1d8f4d2c4f8ca7d2ac9a
Vishnu Gupta <geekmaths0@gmail.com>
2026-07-28 19:31:45 +0530

    Time: 4 ms (20.48%), Space: 47.9 MB (6.09%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        if( n==1 )return 1;
+
+        int []ans = new int[n];
+
+
+        ans[0] = 1;
+        for(int i=1; i<n; i++){
+            
+            boolean need = false;
+            if(ratings[i] > ratings[i-1]) need = true;
+
+            if( need ){
+                ans[i] = Math.max(ans[i],ans[i-1]+1);
+            }
+            else{
+                ans[i] = Math.max(ans[i],1);
+            }
+            
+        }
+
+        for(int i=n-2; i>=0; i--){
+            
+            boolean need = false;
+            
+            if(ratings[i] > ratings[i+1]) need=true;
+
+            if( need ){
+                ans[i] = Math.max(ans[i],ans[i+1]+1);
+            }
+            else{
+                ans[i] = Math.max(ans[i],1);
+            }
+            
+        }
+
+        int sum = 0;
+        for(int e: ans) sum += e;
+        return sum;
+    }
+}
