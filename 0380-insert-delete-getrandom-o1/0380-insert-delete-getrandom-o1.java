4552fc0dde725a746d55f1611a61d2a08a95bb72
Vishnu Gupta <geekmaths0@gmail.com>
2026-07-28 19:06:32 +0530

    Time: 26 ms (76.51%), Space: 100.8 MB (14.06%) - LeetHub

---
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
@@ -0,0 +1,66 @@
+class RandomizedSet {
+    int n ;
+    List<Integer> arr ;
+    Map <Integer,Integer> map ;
+    public RandomizedSet() {
+        n = 0;
+        arr = new ArrayList<>();
+        map = new HashMap<>();
+    }
+    
+    public boolean insert(int val) {
+        if(map.containsKey(val)) return false;
+        map.put(val,n);
+
+        if( arr.size() == n )  arr.add(val);
+        else arr.set(n,val);
+
+        n++;
+
+        // for(int e: arr) System.out.print(e+" ");
+        // System.out.println();
+        // System.out.println("++"+n);
+
+        return true;
+    }
+    
+    public boolean remove(int val) {
+        if(!map.containsKey(val)) return false;
+
+        int idx = map.get(val);
+        int last = n-1;
+
+        if(idx == last) {
+            n--;
+            map.remove(val);
+            return true;
+        }
+
+        arr.set(idx,arr.get(last));
+        n--;
+        map.remove(val);
+
+        map.put(arr.get(last), idx);
+
+        return true;
+    }
+    
+    public int getRandom() {
+
+        // for(int e: arr) System.out.print(e+" ");
+        // System.out.println();
+        // System.out.println("++"+n);
+
+        int res = (int)(Math.random()*n);
+        // System.out.println(res);
+        return arr.get(res);
+    }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
