dd962eeb33257e3fdd95b0502fe8e4a224ac61ed
Vishnu Gupta <geekmaths0@gmail.com>
2026-07-30 10:00:01 +0530

    Time: 3 ms (11.24%), Space: 43.2 MB (47.13%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -1,33 +1,23 @@
 class Solution {
     public int minimumPushes(String word) {
-        int [][]f = new int[26][2];
+        int []f = new int[26];
         char []arr = word.toCharArray();
 
         for(char c: arr){
-             f[c-'a'][0] ++;
-             f[c-'a'][1] = c-'a';
+             f[c-'a'] ++;
         }
 
-        Arrays.sort(f,(a,b)->Integer.compare(b[0],a[0]));
+        Arrays.sort(f);
 
-     
-        // int []revMap = new int[26];
-        // int r = 1;
-        // for(int j=0; j<26; j++){
-        //     if(f[j][0] == 0) continue;
-            
-        //     revMap[f[j][1]] = r++;   // r rep  rank
-        // }
-
-    
         int push = 0;
         int rank = 1;
-        for(int []c : f){
-            if(c[0] == 0)continue;
-            if(rank<=8) push+=1*c[0];
-            else if(rank<=16) push+=2*c[0];
-            else if(rank <=24) push+=3*c[0];
-            else push +=4*c[0];
+        for(int j=25; j>=0; j--){
+            int freq = f[j];
+            if(freq == 0)continue;
+            if(rank<=8) push+=1*freq;
+            else if(rank<=16) push+=2*freq;
+            else if(rank <=24) push+=3*freq;
+            else push +=4*freq;
             rank++;
         }
 
