2c68c4cee96e9bc06bdc7a99af8e1921ef4f9f75
Vishnu Gupta <geekmaths0@gmail.com>
2026-08-01 13:02:18 +0530

    Time: 8 ms (19.08%), Space: 46.9 MB (10.88%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.java
@@ -0,0 +1,18 @@
+class Solution {
+    int sol21(int []arr, int i, int j, int[][]dp){
+        if(i==j) return arr[i];
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int a = arr[i] - sol21(arr,i+1,j,dp);
+        int b = arr[j] - sol21(arr,i,j-1,dp);
+
+        return dp[i][j] = Math.max(a,b);
+    }
+    public boolean stoneGame(int[] piles) {
+        int n = piles.length;
+        int [][]dp = new int[n][n];
+        for(int i=0; i<n; i++) Arrays.fill(dp[i],-1);
+        return sol21(piles,0,n-1,dp)>=0;
+    }
+}
