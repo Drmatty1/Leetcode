377b055a2443f302c566d8cc74013ac41fc49ccf
Vishnu Gupta <geekmaths0@gmail.com>
2026-08-01 13:04:37 +0530

    Time: 0 ms (100%), Space: 42.9 MB (76.82%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -10,9 +10,17 @@ class Solution {
         return dp[i][j] = Math.max(a,b);
     }
     public boolean stoneGame(int[] piles) {
-        int n = piles.length;
-        int [][]dp = new int[n][n];
-        for(int i=0; i<n; i++) Arrays.fill(dp[i],-1);
-        return sol21(piles,0,n-1,dp)>=0;
+        // int n = piles.length;
+        // int [][]dp = new int[n][n];
+        // for(int i=0; i<n; i++) Arrays.fill(dp[i],-1);
+        // return sol21(piles,0,n-1,dp)>=0;
+
+        return true;
     }
 }
+
+//
+// If the array has an  
+// even length
+// ​
+//  , P1 can always force taking either all even-indexed or all odd-indexed elements. By choosing the parity with the larger total sum, P1 is guaranteed at least a tie, so the answer is always true.
