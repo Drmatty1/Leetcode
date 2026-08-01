e90db2e0bb0fd97ee08b0db87f079caba25d9575
Vishnu Gupta <geekmaths0@gmail.com>
2026-08-01 12:41:25 +0530

    Time: 65 ms (28.69%), Space: 42.6 MB (74.96%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,21 +1,33 @@
 class Solution {
-    boolean sol(int []a, int i, int j, long diff){
+    boolean sol1(int []a, int i, int j, long diff){
         if(i==j) return (diff+a[i]>=0);
         if(i>j ) return diff>=0;
 
         boolean p=false, b=false, c=false, d=false;
         // System.out.println(i+" "+j+" "+diff);
 
-        p = sol(a,i+1,j-1,diff+a[i]-a[j]);
+        p = sol1(a,i+1,j-1,diff+a[i]-a[j]);
         if(i+1 < a.length)
-            b = sol(a,i+2,j,diff+a[i]-a[i+1]);
-        c = sol(a,i+1,j-1,diff+a[j]-a[i]);
+            b = sol1(a,i+2,j,diff+a[i]-a[i+1]);
+        c = sol1(a,i+1,j-1,diff+a[j]-a[i]);
         if(j-1 >= 0)
-            d = sol(a,i,j-2,diff+a[j]-a[j-1]);
+            d = sol1(a,i,j-2,diff+a[j]-a[j-1]);
         
         return p&&b || c&&d;
     }
+
+    int sol2(int []arr, int i, int j){
+        if(i==j) return arr[i];
+
+        int a = arr[i] - sol2(arr,i+1,j);
+        int b = arr[j] - sol2(arr,i,j-1);
+
+        return Math.max(a,b);
+    }
+
     public boolean predictTheWinner(int[] nums) {
-        return sol(nums,0,nums.length-1,0);
+        // return sol(nums,0,nums.length-1,0);
+
+        return sol2(nums,0,nums.length-1)>=0;
     }
 }
