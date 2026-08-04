1ab3596aa56b61eadd0a5e46e39d11b39d9b1c53
Vishnu Gupta <geekmaths0@gmail.com>
2026-08-04 09:57:11 +0530

    Time: 315 ms (26.89%), Space: 47.9 MB (42.86%) - LeetHub

--- 1563-stone-game-v/1563-stone-game-v.java
+++ 1563-stone-game-v/1563-stone-game-v.java
@@ -47,6 +47,42 @@ class Solution {
         }
         return dp[i][j] = ans;
     }
+    int sol2( int []arr){
+        int n = arr.length;
+
+        int [][]dp = new int[n][n];
+        for(int i=0; i<n; i++){
+            dp[i][i] = 0;
+        }
+
+        for(int len=1; len<n; len++){
+            for(int i=0; i+len<n; i++){
+                int j=i+len;
+
+                int ans = 0;
+                for(int m=i; m<j; m++){
+                    int lsum = pre[m+1]-pre[i];
+                    int rsum = pre[j+1]-pre[m+1];
+                    int temp = 0;
+                    if(lsum < rsum){
+                        temp = lsum + dp[i][m];
+                    }
+                    else if(lsum > rsum){
+                        temp = rsum + dp[m+1][j];
+                    }
+                    else{
+                        temp = Math.max(
+                            lsum + dp[i][m],
+                            rsum + dp[m+1][j]
+                        );
+                    }
+                    ans = Math.max(ans,temp);
+                }
+                dp[i][j] = ans;
+            }
+        }
+        return dp[0][n-1];
+    }
     public int stoneGameV(int[] stoneValue) {
         int n = stoneValue.length;
         pre = new int[n+1];
@@ -54,8 +90,11 @@ class Solution {
             pre[i+1] = pre[i]+stoneValue[i];
         
         // return sol(0,n-1,stoneValue);
-        int [][]dp = new int[n][n];
-        for(int i=0; i<n; i++)Arrays.fill(dp[i],-1);
-        return sol1(0,n-1,stoneValue,dp);
+
+        // int [][]dp = new int[n][n];
+        // for(int i=0; i<n; i++)Arrays.fill(dp[i],-1);
+        // return sol1(0,n-1,stoneValue,dp);
+
+        return sol2(stoneValue);
     }
 }
