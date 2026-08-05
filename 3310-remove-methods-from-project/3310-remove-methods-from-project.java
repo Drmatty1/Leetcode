33dddb2c1c6e34a4221348a9bb7bd3808c4631f1
Vishnu Gupta <geekmaths0@gmail.com>
2026-08-05 09:38:45 +0530

    Time: 86 ms (41.35%), Space: 287.2 MB (50.96%) - LeetHub

--- 3310-remove-methods-from-project/3310-remove-methods-from-project.java
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.java
@@ -1,4 +1,5 @@
 class Solution {
+
     void dfs(List<List<Integer>> adj, boolean []mark, int k){
         mark[k] = true;
         for(int n: adj.get(k)){
@@ -7,22 +8,65 @@ class Solution {
             }
         }
     }
+
+    void dfs_it(List<List<Integer>> adj, boolean[] mark, int start) {
+        Stack<Integer> st = new Stack<>();
+        st.push(start);
+
+        while (!st.isEmpty()) {
+            int node = st.pop();
+
+            if (mark[node])
+                continue;
+
+            mark[node] = true;
+
+            for (int next : adj.get(node))
+                if (!mark[next])
+                    st.push(next);
+        }
+    }
+
     boolean dfs1(List<List<Integer>> adj,boolean []vis, boolean []mark, int k){
 
         if(mark[k] == true) return true;
 
-        boolean res = false;
         vis[k] = true;
 
-        for(int n: adj.get(k)){
-            if(vis[n] == false){
-                res |= dfs1(adj,vis,mark,n);
-                if(res) break;
+        for (int next : adj.get(k)) {
+            if (!vis[next] && dfs1(adj, vis, mark, next)) {
+                return true;
             }
         }
 
-        return res;
+        return false;
+
     }
+
+    boolean dfs1_it(List<List<Integer>> adj,boolean []vis, boolean []mark, int k){
+
+        Stack<Integer> st = new Stack<>();
+        st.push(k);
+
+         while (!st.isEmpty()) {
+
+            int n = st.pop();
+
+            if (mark[n]) return true;
+
+            vis[n] = true;
+
+            for (int next : adj.get(n)) {
+                if (!vis[next] ) {
+                    st.push(next);
+                }
+            }
+        }
+
+        return false;
+
+    }
+
     public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
         List<List<Integer>> adj = new ArrayList<>();
         for(int i=0; i<n; i++){
@@ -43,6 +87,7 @@ class Solution {
         for(int i=0; i<n; i++)
             if(vis[i] == false && mark[i] == false){
                 ans |= dfs1(adj,vis,mark,i);
+                // ans |= dfs1_it(adj,vis,mark,i);
                 if(ans) break;
             }
        
