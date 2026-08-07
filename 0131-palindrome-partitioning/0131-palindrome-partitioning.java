864fa44b33a6ab7022b1829be1afaf0c375ae349
Vishnu Gupta <geekmaths0@gmail.com>
2026-08-07 18:42:36 +0530

    Time: 7 ms (97.02%), Space: 65.3 MB (58.69%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -8,12 +8,16 @@ class Solution {
         }
         return true;
     }
+
+    //extra optimization
+    boolean [][]dp;
+
     void sol(int i, String s, List<String> temp){
         if(i>=s.length()){
             ans.add(new ArrayList<>(temp));
         }
         for(int j=i; j<s.length(); j++){
-            if(check(s,i,j)) {
+            if(dp[i][j]) {
                 temp.add(s.substring(i,j+1));
                 sol(j+1,s,temp);
                 temp.remove(temp.size()-1);
@@ -23,6 +27,14 @@ class Solution {
     }
     public List<List<String>> partition(String s) {
         ans = new ArrayList<>();
+        
+        dp = new boolean[s.length()][s.length()];
+        for(int i=0; i<s.length(); i++){
+            for(int j=0; j<s.length(); j++){
+                dp[i][j] = check(s,i,j);
+            }
+        }
+
         List<String> temp = new ArrayList<>();
         sol(0,s,temp);
         return ans;
