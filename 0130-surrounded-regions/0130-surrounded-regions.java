537b5040aae9db534b9d248a0d95ca1b4ee3cd1b
Vishnu Gupta <geekmaths0@gmail.com>
2026-08-07 17:49:33 +0530

    Time: 2 ms (72.6%), Space: 48 MB (10.54%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,46 +1,60 @@
 class Solution {
     int r,c;
     int [][]dirs = {{0,1},{0,-1},{1,0},{-1,0}};
-    boolean canEscape(char[][] board, int i, int j, boolean [][]vis){
-        if(i<0 || i>=r || j<0 || j>=c) return true;
-        if(board[i][j] == 'X') return false;
-        if(vis[i][j]) return false;
-        vis[i][j] = true;
 
-        for(int []d: dirs){
-            int ni = i+d[0];
-            int nj = j+d[1];
-            if(canEscape(board,ni,nj,vis)) return true;
-        }
-        return false;
-    }
-    void fill(char[][] board, int i, int j){
+    void mark(char[][] board, int i, int j){
         if(i<0 || i>=r || j<0 || j>=c) return ;
-        if(board[i][j] == 'X') return ;
+        if(board[i][j] != 'O') return ;
 
-        board[i][j] = 'X';
+        board[i][j] = '#';
 
         for(int []d: dirs){
             int ni = i+d[0];
             int nj = j+d[1];
-            fill(board,ni,nj);
+            mark(board,ni,nj);
         }
 
         return ;
     }
+
     public void solve(char[][] board) {
         r = board.length;
         c = board[0].length;
+
+        // marking gaureented safe O as #
         for(int i=0; i<r; i++){
-            for(int j=0; j<c; j++){
-                if(board[i][j] == 'O'){
-                    boolean [][]vis = new boolean[r][c];
-                    if(!canEscape(board,i,j,vis)){
-                        fill(board,i,j);
-                    }
-                }
+            // first col 
+            if(board[i][0] == 'O'){
+                mark(board,i,0);
+            }
+            // last col
+            if(board[i][c-1] == 'O'){
+                mark(board,i,c-1);
             }
         }
+        for(int j=0; j<c; j++){
+            // first row 
+            if(board[0][j] == 'O'){
+                mark(board,0,j);
+            }
+            // last row
+            if(board[r-1][j] == 'O'){
+                mark(board,r-1,j);
+            }
+        }
+
+        for(int i=0; i<r; i++){
+            for(int j=0; j<c; j++){
+                if(board[i][j] == 'O') board[i][j] = 'X';
+            }
+        }
+
+        for(int i=0; i<r; i++){
+            for(int j=0; j<c; j++){
+                if(board[i][j] == '#') board[i][j] = 'O';
+            }
+        }
+
         return ;
     }
 }
