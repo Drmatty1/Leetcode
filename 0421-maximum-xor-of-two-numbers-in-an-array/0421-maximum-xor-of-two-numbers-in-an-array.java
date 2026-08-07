4aec54887088b6b594a7ef9f19f09f22725dae21
Vishnu Gupta <geekmaths0@gmail.com>
2026-08-07 17:18:39 +0530

    Time: 592 ms (80.78%), Space: 242.1 MB (10.14%) - LeetHub

---
+++ 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int findMaximumXOR(int[] nums) {
+        int n = nums.length;
+        int max = 0, mask=0;
+
+        // here we are tring to maximize i'th bit 
+        for(int i=31; i>=0; i--){
+            
+            mask = mask | (1<<i);
+            Set<Integer> set = new HashSet<>();
+            for(int num: nums){
+                set.add(num & mask);
+            }
+
+            // ideal candidate (pref) we want to get
+            int cand = max | (1<<i);
+            // here we want to check prefx^prefy == cand -> o(n^2)
+            // instead use property a^b = c  -> a^c = b;
+            for(int pref: set){
+                if(set.contains(pref^cand)){
+                    max = cand;
+                    break;
+                }
+            }
+        }
+
+        return max;
+    }
+}
