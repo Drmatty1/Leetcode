3232c6a6f53d351d1ec619f85a2857a93f4ade83
Vishnu Gupta <geekmaths0@gmail.com>
2026-08-10 12:03:27 +0530

    Time: 309 ms (28.52%), Space: 47.7 MB (51.06%) - LeetHub

--- 1563-stone-game-v/1563-stone-game-v.java
+++ 1563-stone-game-v/1563-stone-game-v.java
@@ -47,6 +47,8 @@ class Solution {
         }
         return dp[i][j] = ans;
     }
+
+    // bootomm up ->  digaonal traversal
     int sol2( int []arr){
         int n = arr.length;
 
@@ -83,6 +85,44 @@ class Solution {
         }
         return dp[0][n-1];
     }
+
+    // bootom up -> classicl
+    int sol21( int []arr){
+        int n = arr.length;
+
+        int [][]dp = new int[n][n];
+        for(int i=0; i<n; i++){
+            dp[i][i] = 0;
+        }
+
+        for(int i=n-1; i>=0; i--){
+            for(int j=i+1; j<n; j++){
+
+                int ans = 0;
+                for(int m=i; m<j; m++){
+                    int lsum = pre[m+1]-pre[i];
+                    int rsum = pre[j+1]-pre[m+1];
+                    int temp = 0;
+                    if(lsum < rsum){
+                        temp = lsum + dp[i][m];
+                    }
+                    else if(lsum > rsum){
+                        temp = rsum + dp[m+1][j];
+                    }
+                    else{
+                        temp = Math.max(
+                            lsum + dp[i][m],
+                            rsum + dp[m+1][j]
+                        );
+                    }
+                    ans = Math.max(ans,temp);
+                }
+                dp[i][j] = ans;
+            }
+        }
+        return dp[0][n-1];
+    }
+
     public int stoneGameV(int[] stoneValue) {
         int n = stoneValue.length;
         pre = new int[n+1];
@@ -95,6 +135,52 @@ class Solution {
         // for(int i=0; i<n; i++)Arrays.fill(dp[i],-1);
         // return sol1(0,n-1,stoneValue,dp);
 
-        return sol2(stoneValue);
+        // return sol2(stoneValue);
+
+        return sol21(stoneValue);
     }
 }
+
+// best O(n^2 logn) even O(n^2) best
+/**
+class Solution {
+    // returns first index where sum of left half >= sum of right half
+    private int search(int[] pre, int l, int r) {
+        int sum = pre[r+1] - pre[l], L = l;
+        while(l < r) {
+            int m = l + ((r - l) >> 1);
+            if(((pre[m+1] - pre[L]) << 1) >= sum) {
+                r = m;
+            } else {
+                l = m + 1;
+            }
+        }
+        return l;
+    }
+    public int stoneGameV(int[] stoneValue) {
+        int n = stoneValue.length;
+        int[] pre = new int[n+1];
+        for(int i = 1; i <= n; i++) {
+            pre[i] = pre[i-1] + stoneValue[i-1];
+        }
+        int[][] dp = new int[n][n], left = new int[n][n], right = new int[n][n];
+        for(int i = 0; i < n; i++) {
+            left[i][i] = right[i][i] = stoneValue[i];
+        }
+        for(int l = 1; l < n; l++) {
+            for(int i = 0; i < n-l; i++) {
+                int j = i+l, k = search(pre, i, j);
+                int sum = pre[j+1] - pre[i], leftHalf = pre[k+1] - pre[i];
+                if((leftHalf << 1) == sum) {    // equal parts
+                    dp[i][j] = Math.max(left[i][k], right[k+1][j]);
+                } else {    // left half > right half
+                    dp[i][j] = Math.max(k == i ? 0 : left[i][k-1], k == j ? 0 : right[k+1][j]);
+                }
+                left[i][j] = Math.max(left[i][j-1], sum + dp[i][j]);
+                right[i][j] = Math.max(right[i+1][j], sum + dp[i][j]);
+            }
+        }
+        return dp[0][n-1];
+    }
+}
+ */
