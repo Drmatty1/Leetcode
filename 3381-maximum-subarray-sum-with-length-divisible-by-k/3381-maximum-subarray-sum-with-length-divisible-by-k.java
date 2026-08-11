97634ca19a2778da672d4ccd198fe28880532139
Vishnu Gupta <geekmaths0@gmail.com>
2026-08-11 22:30:21 +0530

    Time: 2199 ms (5.36%), Space: 135.2 MB (100%) - LeetHub

---
+++ 3381-maximum-subarray-sum-with-length-divisible-by-k/3381-maximum-subarray-sum-with-length-divisible-by-k.java
@@ -0,0 +1,19 @@
+class Solution {
+    public long maxSubarraySum(int[] nums, int k) {
+        int n = nums.length;
+        long []pre = new long[n+1];
+        Arrays.fill(pre,Long.MAX_VALUE>>1);
+        pre[0] = 0;
+        long sum = 0;
+        long ans = Long.MIN_VALUE;
+
+        for(int i=1; i<=n; i++){
+            sum += nums[i-1];
+            if(i>=k)
+                ans = Math.max(ans,sum-pre[i%k]);
+            pre[i%k] = Math.min(pre[i%k],sum);
+            System.out.println(pre[i%k]);
+        }
+        return ans;
+    }
+}
