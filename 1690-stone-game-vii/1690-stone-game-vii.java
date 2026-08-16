9c94dd2b0ac3ba0b5cb97a77b24ae2ba3df607c5
Vishnu Gupta <geekmaths0@gmail.com>
2026-08-16 09:47:49 +0530

    Time: 88 ms (49.33%), Space: 62.1 MB (79.36%) - LeetHub

---
+++ 1690-stone-game-vii/1690-stone-game-vii.java
@@ -0,0 +1,38 @@
+class Solution {
+    int []pre;
+    int [][]dp;
+    int sol(int[]a, int i, int j){
+        if(i==j) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int maxDiff = 0;
+        // pick j
+        maxDiff = pre[j]-pre[i] - sol(a,i,j-1);
+
+        //pick i
+        maxDiff = Math.max(maxDiff, pre[j+1]-pre[i+1] - sol(a,i+1,j));
+
+        return dp[i][j] = maxDiff;
+    }
+    public int stoneGameVII(int[] stones) {
+        int n = stones.length;
+        pre = new int[n+1];
+        for(int i=0; i<n; i++) pre[i+1]=pre[i]+stones[i];
+        
+        dp = new int[n][n];
+        for(int i=0; i<n; i++) Arrays.fill(dp[i],-1);
+        return sol(stones,0,n-1);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
