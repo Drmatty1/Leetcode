0ee1f6d95668d0143f6ff68b6c9312fd0074ebfc
Vishnu Gupta <geekmaths0@gmail.com>
2026-08-16 11:23:17 +0530

    Time: 52 ms (5.17%), Space: 112.3 MB (5.17%) - LeetHub

---
+++ 1872-stone-game-viii/1872-stone-game-viii.java
@@ -0,0 +1,25 @@
+class Solution {
+    int []pre;
+    int n;
+    int [][]dp ;
+
+    int sol(int []a, int i,boolean start){
+        if(i==n-1) return pre[i+1];
+        if(dp[i][start?1:0]!=Integer.MIN_VALUE) return dp[i][start?1:0];
+        int max = sol(a,i+1,true);
+        if(start) 
+            max = Math.max(max,pre[i+1]-sol(a,i,false));
+
+        return dp[i][start?1:0] = max;
+    }
+    public int stoneGameVIII(int[] stones) {
+        n = stones.length;
+        pre = new int[n+1];
+        for(int i=0; i<n; i++) pre[i+1] = pre[i]+stones[i];
+
+        dp = new int[n][2];
+        for(int i=0; i<n; i++)
+            Arrays.fill(dp[i],Integer.MIN_VALUE);
+        return sol(stones,1,true);
+    }
+}
