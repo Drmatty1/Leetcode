83cf2620c8d74812ff75d383d9d9b65cde1a3501
Vishnu Gupta <geekmaths0@gmail.com>
2026-08-16 12:01:04 +0530

    Time: 9 ms (10.14%), Space: 114.7 MB (82.43%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.java
@@ -0,0 +1,54 @@
+class Solution {
+    boolean sol(int curr,int a, int b, int c, int n){
+        n--;
+        int turn = 0; //bob
+        while(n-->0){
+            if(curr==1){
+                if(a>0) a--;
+                else if(b>0){
+                    b--;
+                    curr++;
+                }
+                else if(c>0){
+                    return turn==0?true:false;
+                }
+                else{
+                    return false;
+                }
+            }
+            else if(curr==2){
+                if(a>0) a--;
+                else if(c>0){
+                    c--;
+                    curr--;
+                }
+                else if(b>0){
+                    return turn==0?true:false;
+                }
+                else{
+                    return false;
+                }
+            }
+            turn = 1-turn;
+        }
+        return false;
+    }
+    public boolean stoneGameIX(int[] stones) {
+
+        int a=0,b=0,c=0;
+        int n = stones.length;
+
+        for(int s: stones) {
+            if(s%3 == 0) a++;
+            else if(s%3 == 1) b++;
+            else c++;
+        }
+
+        // case 1
+        boolean case1 = b>0?sol(1,a,b-1,c,n):false;
+        boolean case2 = c>0?sol(2,a,b,c-1,n):false;
+
+        return case1||case2;
+
+    }
+}           
