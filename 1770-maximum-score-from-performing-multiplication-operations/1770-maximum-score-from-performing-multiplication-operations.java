492b22b170caa02f8e8b73a7f1a1c3749c9f6b46
Vishnu Gupta <geekmaths0@gmail.com>
2026-08-16 13:16:52 +0530

    Time: 14 ms (51.72%), Space: 64.3 MB (74.14%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
@@ -3,6 +3,7 @@ class Solution {
     int []mul;
 
     Map<String,Integer> dp;
+    int [][]dp1 ;
 
     int sol(int i, int l, int r){
         if(i==mul.length) return 0;
@@ -15,6 +16,7 @@ class Solution {
         return max;
     }
 
+    //O(m^3) , m=10^2
     int sol1(int i, int l, int r){
         if(i==mul.length) return 0;
 
@@ -30,14 +32,35 @@ class Solution {
         return max;
     }
 
+
+    int sol_OP(int i, int l){
+        if(i==mul.length) return 0;
+        if(dp1[i][l] != Integer.MIN_VALUE ) return dp1[i][l];
+
+        int r =  (arr.length-1) - (i - l);
+        int max = Math.max(
+            arr[l]*mul[i]+sol_OP(i+1,l+1),
+            arr[r]*mul[i]+sol_OP(i+1,l)
+        );
+
+        return dp1[i][l] = max;
+    }
+
     public int maximumScore(int[] nums, int[] multipliers) {
         arr = nums;
         mul = multipliers;
 
-        // return sol(0,0,nums.length-1);
+        // O(m^3) BruteForce
+        // dp = new HashMap<>();
+        // return sol1(0,0,nums.length-1);
 
-        dp = new HashMap<>();
-        return sol1(0,0,nums.length-1);
-
+        //O(m^2) ,State compressed (i.e. r = nums.length-i-1)
+        // return sol_OP(0,0);
+        
+        int m = multipliers.length;
+        dp1 = new int[m][m];
+        for(int i=0; i<m ;i++)
+            Arrays.fill(dp1[i],Integer.MIN_VALUE);
+        return sol_OP(0,0);
     }
 }
