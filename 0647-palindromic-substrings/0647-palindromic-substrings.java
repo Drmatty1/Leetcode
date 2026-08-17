8051e757f0a19bdb39e57344fe8a9eec60bd7919
Vishnu Gupta <geekmaths0@gmail.com>
2026-08-17 19:40:55 +0530

    Time: 8 ms (42.37%), Space: 46.4 MB (26.28%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,15 +1,17 @@
 class Solution {
     int count(char []a, int i, int j){
-        int n = a.length;
 
+        int n = a.length;
         int count = 0;
+
         while(i>=0 && j<n){
             if(a[i--] == a[j++]) count++;
             else break;
         }
+
         return count;
     }
-    public int countSubstrings(String s) {
+    int sol_OP(String s) {
         char []a = s.toCharArray();
         int n = a.length;
         int ans = 0;
@@ -18,5 +20,31 @@ class Solution {
             ans += 1+count(a,i-1,i+1); //odd length;
         }
         return ans;
+    }
+
+    int sol2(String s){
+
+        int n  = s.length();
+        boolean [][]dp = new boolean[n][n];
+
+        for(int i=0; i<n; i++) dp[i][i] = true;
+
+        char []a = s.toCharArray();
+        int count = 0 ;
+
+        for(int i=n-1; i>=0; i--){
+            for(int j=i; j<n; j++){
+                if(a[i] == a[j] && ( j-i < 2 || dp[i+1][j-1] ) ){
+                    count++;
+                    dp[i][j] = true;
+                }
+            }
+        }
+
+        return count;
+
+    }
+    public int countSubstrings(String s) {
+        return sol2(s);
     }
 }
