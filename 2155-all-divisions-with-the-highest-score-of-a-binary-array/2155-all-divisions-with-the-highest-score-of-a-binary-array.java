1e2b5338e3e3e48f0ad8d4241f854a0c47d77e4f
Vishnu Gupta <geekmaths0@gmail.com>
2026-08-18 17:06:53 +0530

    Time: 23 ms (31.07%), Space: 270.2 MB (9.71%) - LeetHub

--- 2155-all-divisions-with-the-highest-score-of-a-binary-array/2155-all-divisions-with-the-highest-score-of-a-binary-array.java
+++ 2155-all-divisions-with-the-highest-score-of-a-binary-array/2155-all-divisions-with-the-highest-score-of-a-binary-array.java
@@ -1,10 +1,10 @@
 class Solution {
     public List<Integer> maxScoreIndices(int[] nums) {
         int n = nums.length;
-        int []pre = new int[n+1];
-        for(int i=0; i<n; i++){
-            pre[i+1] = pre[i]+(nums[i]==0?1:0);
-        }
+        // int []pre = new int[n+1];
+        // for(int i=0; i<n; i++){
+        //     pre[i+1] = pre[i]+(nums[i]==0?1:0);
+        // }
 
         int []suf = new int[n+1];
         for(int i=n-1; i>=0; i--){
@@ -12,13 +12,17 @@ class Solution {
         }
 
         int max = 0;
+        int pre = 0;
         for(int i=0; i<=n; i++){
-            max = Math.max(max,pre[i]+suf[i]);
+            if(i>0) pre += (nums[i-1]==0?1:0);
+            max = Math.max(max,pre+suf[i]);
         }
 
         List<Integer> ans = new ArrayList<>();
+        pre = 0;
         for(int i=0; i<=n; i++){
-            if(pre[i]+suf[i] == max) ans.add(i);
+            if(i>0) pre += (nums[i-1]==0?1:0);
+            if(pre+suf[i] == max) ans.add(i);
         }
 
         return ans;
