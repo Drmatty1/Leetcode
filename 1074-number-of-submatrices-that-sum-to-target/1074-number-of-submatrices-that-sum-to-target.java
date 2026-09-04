9b25dbe97c6bdd3a163775052f28a7e943e758d1
Vishnu Gupta <geekmaths0@gmail.com>
2026-09-05 00:37:00 +0530

    Time: 100 ms (90.96%), Space: 47.3 MB (25.42%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.java
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.java
@@ -1,5 +1,6 @@
 class Solution {
     Map<Integer,Integer> map = new HashMap<>();
+
     int subarraySum(int []a, int t ){
         int n = a.length;
 
@@ -17,7 +18,7 @@ class Solution {
         return ans;
 
     }
-    public int numSubmatrixSumTarget(int[][] matrix, int target) {
+    int sol1(int[][] matrix, int target) {
         
         int r = matrix.length, c = matrix[0].length;
 
@@ -37,5 +38,37 @@ class Solution {
         }
 
         return ans;
+    }
+
+    public int numSubmatrixSumTarget(int[][] matrix, int target) {  
+        int r = matrix.length, c = matrix[0].length;
+
+        int[][] pre = new int[r][c + 1];
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                pre[i][j + 1] = pre[i][j] + matrix[i][j];
+            }
+        }
+
+        int ans = 0;
+
+        for(int c1 = 0; c1<c; c1++){
+            for(int c2 = c1; c2<c; c2++){
+                
+                int runningSum = 0;
+                map.clear();
+                map.put(0,1);
+
+                for(int i=0; i<r; i++){
+                    runningSum += pre[i][c2+1]-pre[i][c1];
+                    ans += map.getOrDefault(runningSum-target,0);
+                    map.put(runningSum,map.getOrDefault(runningSum,0)+1);
+                }
+
+            }
+        }
+
+        return ans;
+        
     }
 }
