3c053f1b2732991c2702c07ac8ebf372ec1ccb54
Vishnu Gupta <geekmaths0@gmail.com>
2026-09-15 16:22:41 +0530

    Time: 44 ms (47.76%), Space: 47.6 MB (42.81%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -1,23 +1,22 @@
 class Solution {
     int n ;
-    // boolean [][]dp;
-    int []dp1;
+    boolean [][]isPal;
+    int []dp;
     String str;
 
     int sol(int i){
         if(i>=n) return 0;
 
-        if(dp1[i] != -1) return dp1[i];
+        if(dp[i] != -1) return dp[i];
         int ans = 2000;
 
         for(int j = i; j<n; j++){
-            if(check(str,i,j)){
-                // System.out.println(i+"-"+j);
+            if(isPal[i][j]){
                 ans = Math.min(1+sol(j+1),ans);
             }
         }
 
-        return dp1[i] = ans;
+        return dp[i] = ans;
     }
 
     boolean check(String s, int i, int j){
@@ -32,17 +31,20 @@ class Solution {
     public int minCut(String s) {
         n = s.length();
 
-        // dp = new boolean[n][n];
-        dp1 = new int[n];
+        isPal = new boolean[n][n];
+        dp = new int[n];
         str = s;
 
-        Arrays.fill(dp1,-1);
+        Arrays.fill(dp,-1);
 
-        // for(int i=0; i<n; i++){
-        //     for(int j=0; j<n; j++){
-        //         dp[i][j] = check(s,i,j);
-        //     }
-        // }
+        for(int len=1; len<=n; len++){
+            for(int i =0; i<=n-len; i++){
+                int j = i+len-1;
+                if(s.charAt(i) == s.charAt(j)) 
+                    isPal[i][j] = (len <= 2) || isPal[i + 1][j - 1];
+            }
+        }
+        
 
         return sol(0)-1;
     }
