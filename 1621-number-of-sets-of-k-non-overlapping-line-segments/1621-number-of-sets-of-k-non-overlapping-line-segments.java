582ec18c515a1f02bb80b627daa74bb36fc8cecd
Vishnu Gupta <geekmaths0@gmail.com>
2026-09-16 16:13:59 +0530

    Time: 31 ms (63.23%), Space: 62.6 MB (73.55%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.java
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.java
@@ -1,7 +1,7 @@
 class Solution {
     int n;
     Integer [][][]memo;
-    int mod = 1000000007;
+    int MOD = 1000000007;
     int sol(int i, int k, int currSeg){
         
         // k is remK
@@ -21,11 +21,59 @@ class Solution {
             ans += sol(i+1,k,0);    //skip
         }
 
-        return memo[i][k][currSeg] = ans%mod;
+        return memo[i][k][currSeg] = ans%MOD;
     }
+
+
+    int sol2(int n, int k) {
+        long[][] dp = new long[n][k + 1];
+
+        // 0 segments -> exactly 1 way (draw nothing)
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int j = 1; j <= k; j++) {
+            long sum = 0;
+            for (int i = 1; i < n; i++) {
+                // Add ways where the j-th segment starts at (i - 1)
+                sum = (sum + dp[i - 1][j - 1]) % MOD;
+
+                // Option 1: Segment does not end at i -> dp[i - 1][j]
+                // Option 2: Segment ends at i -> sum
+                dp[i][j] = (dp[i - 1][j] + sum) % MOD;
+            }
+        }
+
+        return (int) dp[n - 1][k];
+    }
+
+
+    long quickPow(long a, long e) {
+        long result = 1;
+        while (e > 0) {
+            if ((e & 1) != 0) result = (result * a) % MOD;
+            a = (a * a) % MOD;
+            e >>= 1;
+        }
+        return result;
+    }
+    int solOP(int n, int k) {
+        int m = 2 * k;
+        long numerator = 1, denominator = 1;
+        for (int i = 1; i <= m; i++) {
+            numerator = (numerator * (n + k - i)) % MOD;
+            denominator = (denominator * i) % MOD;
+        }
+        return (int) ((numerator * quickPow(denominator, MOD - 2)) % MOD);
+    }
+
+
     public int numberOfSets(int n, int k) {
         this.n = n;
-        memo = new Integer[n][k+1][2];
-        return sol(0,k,0);
+        // memo = new Integer[n][k+1][2];
+        // return sol(0,k,0);
+
+        return sol2(n,k);
     }
 }
